4f256b7dcd9562a127112c25b022532685b11272
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-02-26 13:19:33 +0000

    Added functions () and create()

--- trunk/bbmvc/includes/dispatcher_functions.php
+++ trunk/bbmvc/includes/dispatcher_functions.php
@@ -9,28 +9,63 @@
  *
  *    $Id$
  *
- * ************************************************************************* */
+ * *************************************************************************
 
-/* * *************************************************************************
- *
- *    This program is Free Software; you can redistribute it and/or
- *    modify it under the terms of the GNU General Public License
- *    as published by the Free Software Foundation; either version 2
- *    of the License, or (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *    GNU General Public License for more details.
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place - Suite 330,
- *    Boston, MA  02111-1307, USA.
- *
- * ************************************************************************* */
+    Copyright (c) 2006, BMR Soft srl, ICE Control srl
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of the <organization> nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* History (Start).                                                          */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*                                                                           */
+/* Date         Name    Reason                                               */
+/* ------------------------------------------------------------------------- */
+/* 26.02.2014           Added import() and create() functions                */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* History (END).                                                            */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 if (!defined('_VALID_ACCESS')) {
     throw new Execption('Access denied!');
+}
+
+function import($name) {
+    $pieces = explode('.', $name);
+    $class = array_pop($pieces);
+
+    $path = implode('/', $pieces) . '/class.' . $class . '.php' ;
+    require_once $path;
+}
+
+function construct($name, $singleton = false) {
+    $class = str_replace('.', '_', $name);
+    if (!$singleton) {
+        return new $class;
+    }
+    return $class();
 }
 
 /**
@@ -62,35 +97,35 @@ function checkVariables(ReflectionClass $class, Security $security, $array) {
 
 /**
  *
- * @param array $params 
+ * @param array $params
  * @return null
  */
 function redirect(array $params) {
 
-    $url_params = "";
+    $url_params = '';
     foreach ($params as $key => $value) {
 
         if (is_array($value)) {
             //arrays are also possible, in this case, we are "expanding" the array
             foreach ($value as $sub_key => $sub_value) {
-                $url_params .= $key . "[" . $sub_key . "]=" . urlencode($sub_value) . "&";
+                $url_params .= $key . '[' . $sub_key . ']=' . urlencode($sub_value) . '&';
             }
         } else {
-            $url_params .= $key . "=" . urlencode($value) . "&";
+            $url_params .= $key . '='. urlencode($value) .'&';
         }
     }
 
-    //removing the last ampersend	
+    //removing the last ampersend
     $url_params = substr($url_params, 0, strlen($url_params) - 1);
 
-    $url = _URL_MAIN . "/" . _FILE_MAIN . "?" . $url_params;
+    $url = _URL_MAIN . '/' . _FILE_MAIN . '?' . $url_params;
 
 
-    //saving session...	
+    //saving session...
     session_write_close();
 
-    header("Location: $url");
-    die();
+    header('Location: ' . $url);
+    die;
 }
 
 /**
@@ -99,55 +134,53 @@ function redirect(array $params) {
  */
 function getSeoUrl(array $params) {
 
-    if (isset($params["href"])) {
-        return _URL_MAIN . "/" . $params["href"];
+    if (isset($params['href'])) {
+        return _URL_MAIN . '/' . $params['href'];
     }
 
-    if (empty($params["module"])) {
-        throw new Exception("getSeoUrl - no module");
+    if (empty($params['module'])) {
+        throw new Exception('getSeoUrl - no module');
         return;
     }
 
-    $href = _FILE_MAIN . "?"; //URL to be used if SEO is not enabled.            
+    $href = _FILE_MAIN . '?'; // URL to be used if SEO is not enabled.
     foreach ($params as $key => $value) {
-        if (substr($key, 0, 4) == "seo_") { //ignoring seo_* params
+        if (substr($key, 0, 4) == 'seo_') { // ignoring seo_* params
             continue;
         }
-        $href .= "$key=$value&amp;";
+        $href .= $key . '=' .$value . '&amp;';
     }
 
     $href = substr($href, 0, strlen($href) - 5);
-    //done building $href
-
+    // done building $href
 
     if (!_USE_SEO_LINKS) {
-        return _URL_MAIN . "/" . $href;
+        return _URL_MAIN . '/' . $href;
     }
 
-    if (empty($params["action"])) {
-        $params["action"] = "defaultAction";
+    if (empty($params['action')) {
+        $params['action'] = 'defaultAction';
     }
 
+    // calling the seo functions of the module
+    $seo_class = $params['module'] . 'Seo';
 
-    //calling the seo functions of the module
-    $seo_class = $params["module"] . "Seo";
-
-    if (is_file(_DIR_MODULES . "/$params[module]/class.$seo_class.php")) { //module file check 		
-        include_once(_DIR_MODULES . "/$params[module]/class.$seo_class.php");
+    if (is_file(_DIR_MODULES . '/' . $params['module'] . '/class.' . $seo_class . '.php')) { // module file check
+        include_once(_DIR_MODULES . '/' . $params['module'] . '/class.' . $seo_class . '.php');
         try {
             $class = new ReflectionClass($seo_class); //throws exception if the class is not existing
-            $action = "seo_" . $params["action"];
+            $action = 'seo_' . $params['action'];
             $method = $class->getMethod($action); //check for public method
             if ($method->isPublic()) {
                 $obj = new $seo_class();
-                return _URL_MAIN . "/" . $obj->$action($params); //calling the seo method
+                return _URL_MAIN . '/' . $obj->$action($params); //calling the seo method
             }
         } catch (Exception $e) {
-            //no seo_class or method not existing
+            // no seo_class or method not existing
         }
     }
 
-    return _URL_MAIN . "/" . $href;
+    return _URL_MAIN . '/' . $href;
 }
 
 // EOF
