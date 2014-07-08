970c61ccca4396ba1270e362dc60e5964726a817
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-07-08 14:31:14 +0000

    Pattern Matching added

---
+++ trunk/bbmvc/includes/libraries/class.PatternMatching.php
@@ -0,0 +1,26 @@
+<?php
+
+trait PatternMatching {
+    private function match_entry(IDefault $object, callable $func) {
+        if ($object == $this) {
+            $func($this);
+            return true;
+        }        
+
+        if ($object->is_default() && (gettype($this) == gettype($object))) {
+            $func($this);
+            return true;
+        }
+        return false;
+    }
+
+    public function match(array $entries)
+    {
+        foreach ($entries as $entry) {
+            list($object, $callback) = $entry;
+            if ($this->match_entry($object, $callback)) {
+                break;
+            }
+        }
+    }
+}
