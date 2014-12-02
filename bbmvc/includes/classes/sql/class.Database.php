ec9b4e0f7079deffdc858e94b4c7e1743cc16ce6
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-02 13:08:47 +0000

    added several methods to sql\Database class

--- bbmvc/includes/classes/sql/class.Database.php
+++ bbmvc/includes/classes/sql/class.Database.php
@@ -17,16 +17,71 @@ class Database {
         implements java.lang.Cloneable 
    */
    
+  /** @var string $databaseName */
+  private $databaseName;
+  
+  /** @var string $hostName */
+  private $hostName;
+  
+  /** @var string $password */
+  private $password;
+  
+  /** @var integer $port */
+  private $port;
+  
+  /** @var string $userName */
+  private $userName;
+   
   /**
    * @param string $user
    * @param string $password
    * @return boolean
    *
-   * @proto: public final boolean open(java.lang.String user, java.lang.String password)
-   * @proto: public final boolean open()   
+   * @proto public final boolean open(java.lang.String user, java.lang.String password)
+   * @proto public final boolean open()   
    */
   public final function open($user = null, $password = null) {
 
+  }
+  
+  /**
+   * @param string $name
+   * @proto public final void setDatabaseName(java.lang.String name) 
+   */
+  public final function setDatabaseName($name) {
+    $this->databaseName = $name;
+  }
+  
+  /* 
+   * @proto public final void setHostName(java.lang.String host) 
+   */
+  public final function setHostName($host) {
+    $this->hostName = $host;
+  }
+  
+  /**
+   * @param string $password
+   * @proto public final void setPassword(java.lang.String password)
+   */
+  public final function setPassword($password) {
+    $this->password = $password;
+  }
+  
+  /**
+   * @param integer $p
+   *
+   * @proto public final void setPort(int p)
+   */
+  public final function setPort($p) {
+    $this->port = intval($p);
+  }
+  
+  /**
+   * @param string $name
+   * @proto public final void setUserName(java.lang.String name)
+   */
+  public final function setUserName($name) {
+    $this->userName = $name;
   }
 }
 
