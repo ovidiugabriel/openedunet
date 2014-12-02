0c63b157e7a5a05ac26402cb9380acaf80732bed
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-02 12:45:43 +0000

    added header comment

--- bbmvc/includes/classes/class.Driver.php
+++ bbmvc/includes/classes/class.Driver.php
@@ -1,7 +1,18 @@
 <?php
 
+/**
+ * Design inspired by: http://qt-jambi.org/
+ * Documentation of Java version at: http://doc.qt.digia.com/qtjambi-4.5.2_01/index.html
+ * See: Package com.trolltech.qt.sql
+ */
 namespace sql;
 
-/** Abstract base class for accessing specific SQL databases */
+/** 
+ * Abstract base class for accessing specific SQL databases 
+ */
 abstract class Driver {
+  /* 
+    public abstract class QSqlDriver
+      extends QObject
+  */
 }
