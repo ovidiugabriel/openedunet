09cea748a0496fe8c66ebeaf808d6be8264265f0
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-02 12:50:24 +0000

    added other classes

--- trunk/bbmvc/includes/classes/sql/class.Database.php
+++ trunk/bbmvc/includes/classes/sql/class.Database.php
@@ -4,6 +4,9 @@
  * Design inspired by: http://qt-jambi.org/
  * Documentation of Java version at: http://doc.qt.digia.com/qtjambi-4.5.2_01/index.html
  * See: Package com.trolltech.qt.sql 
+ *
+ * IMPORTANT!
+ * >>> IT IS NOT ALLOWED TO COPY THE IMPLEMENTATION OF THE QT-JAMBI. <<<
  */
 namespace sql;
 
@@ -13,4 +16,19 @@ class Database {
         extends QtJambiObject
         implements java.lang.Cloneable 
    */
+}
+
+class Location {
+}
+
+class NumericalPrecisionPolicy {
+}
+
+class ParamType {
+}
+
+class ParamTypeFlag {
+}
+
+class TableType {
 }
