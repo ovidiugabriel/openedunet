aed310e58523cd377582cc4f980c6562cc752c56
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-02 12:44:54 +0000

    added header

--- bbmvc/includes/classes/sql/class.Database.php
+++ bbmvc/includes/classes/sql/class.Database.php
@@ -1,7 +1,16 @@
 <?php
 
+/** 
+ * Design inspired by: http://qt-jambi.org/
+ * Documentation of Java version at: http://doc.qt.digia.com/qtjambi-4.5.2_01/index.html
+ * See: Package com.trolltech.qt.sql 
+ */
 namespace sql;
 
 /** Represents a connection to a database */
-class Database {
+class Database { 
+  /* public class QSqlDatabase
+        extends QtJambiObject
+        implements java.lang.Cloneable 
+   */
 }
