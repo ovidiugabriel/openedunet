617716e5d7edab61a0a1609a91b2227508b83e35
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-02 12:54:31 +0000

    added  Result_BindingSyntax and  Result_VirtualHookOperation

--- trunk/bbmvc/includes/classes/sql/class.Result.php
+++ trunk/bbmvc/includes/classes/sql/class.Result.php
@@ -5,3 +5,9 @@ namespace sql;
 /** Abstract interface for accessing data from specific SQL databases */
 abstract class Result {
 }
+
+class Result_BindingSyntax {
+}
+
+class Result_VirtualHookOperation {
+}
