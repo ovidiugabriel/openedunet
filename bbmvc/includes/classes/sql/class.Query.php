6f4d425cca19203691b9893d4e059ae2efc81bab
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-05 16:11:19 +0000

    added Query_BatchExecutionMode

--- bbmvc/includes/classes/sql/class.Query.php
+++ bbmvc/includes/classes/sql/class.Query.php
@@ -9,4 +9,15 @@ class Query {
 }
 
 class Query_BatchExecutionMode {
+  const ValuesAsColumns = 0;
+  const ValuesAsRows    = 1;
+
+  static public function values()
+  {
+    return array(
+      'ValuesAsColumns' => self::ValuesAsColumns,
+      'ValuesAsRows'    => self::ValuesAsRows,
+    );
+  }
+
 }
