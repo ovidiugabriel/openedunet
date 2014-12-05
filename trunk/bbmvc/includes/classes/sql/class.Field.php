d18693fea1455a40b4219abb987e7c1b47982d0a
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-05 16:05:09 +0000

    add Field_RequiredStatus

--- trunk/bbmvc/includes/classes/sql/class.Field.php
+++ trunk/bbmvc/includes/classes/sql/class.Field.php
@@ -12,4 +12,19 @@ class Field_RequiredStatus {
   const Optional = 0;
   const Required = 1;
   const Unknown  = 2;
+
+  static public function values()
+  {
+    return array(
+      'Optional' => self::Optional,
+      'Required' => self::Required,
+      'Unknown'  => self::Unknown,
+    );
+  }
+
+  static public function valueOf($name)
+  {
+    $values = self::values();
+    return $values[$name];
+  }  
 }
