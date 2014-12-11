027e05a20d50b836bc65966ecefacc584a4cda06
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-11 07:55:03 +0000

    added ANONYMOUS_FUNCTIONS

--- bbmvc/includes/feature_test_macros.php
+++ bbmvc/includes/feature_test_macros.php
@@ -5,3 +5,8 @@
 // Are testing the availability of a specific language feature
 // without the need for the user to test the version of PHP each time
 // when a feature availability is tested
+
+// 5.3.0 - Anonymous functions become available. 
+if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
+    define ('ANONYMOUS_FUNCTIONS', 1);
+}
