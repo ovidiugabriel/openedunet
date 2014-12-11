aeccfc87a19b1ec58eba53d51e2252db2aab4075
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2014-12-11 07:47:54 +0000

    Added Feature test macros file

---
+++ bbmvc/includes/feature_test_macros.php
@@ -0,0 +1,7 @@
+<?php
+
+// Feature test macros
+
+// Are testing the availability of a specific language feature
+// without the need for the user to test the version of PHP each time
+// when a feature availability is tested
