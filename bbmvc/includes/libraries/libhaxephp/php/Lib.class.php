901edf0b5305411adac8bd2bf6da0a16487ccede
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2015-02-04 17:21:32 +0000

    tab -> space

--- bbmvc/includes/libraries/libhaxephp/php/Lib.class.php
+++ bbmvc/includes/libraries/libhaxephp/php/Lib.class.php
@@ -3,9 +3,9 @@
 /* Copyright (C) 2014 Haxe Foundation - Do not change */
 
 class php_Lib {
-	public function __construct(){}
-	static function hprint($v) {
-		echo(Std::string($v));
-	}
-	function __toString() { return 'php.Lib'; }
+    public function __construct(){}
+    static function hprint($v) {
+        echo(Std::string($v));
+    }
+    public function __toString() { return 'php.Lib'; }
 }
