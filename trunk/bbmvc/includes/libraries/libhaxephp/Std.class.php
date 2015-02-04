96938e88d1b39ae2788d41b1163060056c23b251
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2015-02-04 17:08:00 +0000

    Std.class.php

---
+++ trunk/bbmvc/includes/libraries/libhaxephp/Std.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Copyright (C) 2014 Haxe Foundation - Do not change */
+
+class Std {
+	public function __construct(){}
+	static public function string($s) {
+		return _hx_string_rec($s, "");
+	}
+	public function __toString() { return 'Std'; }
+}
