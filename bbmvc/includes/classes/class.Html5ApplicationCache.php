6b1ed0c526e7b3980f3d373003b3a1f53135b083
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2015-02-27 20:33:55 +0000

    added comments

--- bbmvc/includes/classes/class.Html5ApplicationCache.php
+++ bbmvc/includes/classes/class.Html5ApplicationCache.php
@@ -6,16 +6,40 @@
 class Html5ApplicationCache {
 }
 
+/** 
+ * Once a file is cached, the browser will continue to show the cached version, 
+ * even if you change the file on the server. 
+ *
+ * To ensure the browser updates the cache, you need to change the manifest file.
+ * This is done using CacheManifest::version member.
+ */
 class CacheManifest {
-    // Files listed under this header will be cached after they are downloaded for the first time
+    /** 
+     * A version string that must be updated each time the cache contents changes 
+     * @var string
+     */
+    private $version;
+
+    /**
+     * Files listed under this header will be cached after they are downloaded for the first time
+     * @var array
+     */
     private $cache_manifest = array();
   
-    // Files listed under this header require a connection to the server, and will never be cached
+    /**
+     * Files listed under this header require a connection to the server, and will never be cached
+     * @var array
+     */
     private $network        = array();
   
-    // Files listed under this header specifies fallback pages if a page is inaccessible
+    /**
+     * Files listed under this header specifies fallback pages if a page is inaccessible
+     * @var array
+     */
     private $fallback       = array();
     
     public function print() {
     }
 }
+
+// EOF
