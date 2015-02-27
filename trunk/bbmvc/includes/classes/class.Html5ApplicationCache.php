845a7723330e2da325fd5bb736c3eac8a1b390df
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2015-02-27 20:39:25 +0000

    added copyright header

--- trunk/bbmvc/includes/classes/class.Html5ApplicationCache.php
+++ trunk/bbmvc/includes/classes/class.Html5ApplicationCache.php
@@ -1,5 +1,36 @@
 <?php
 
+/* * *************************************************************************
+ *  Copyright (c) 2006, BMR Soft srl, ICE Control srl
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      # Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *          
+ *      # Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *          
+ *      # Neither the name of the BMR Soft srl, ICE Control srl nor the
+ *        names of its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL BMR Soft srl, or ICE Control srl BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ************************************************************************* */
+
 /**
  * HTML5 Application Cache file generator
  */
@@ -39,6 +70,18 @@ class CacheManifest {
     private $fallback       = array();
     
     public function print() {
+        println('CACHE MANIFEST');
+        println('# ' . $this->version);
+
+        println('NETWORK:');
+        foreach ($this->network as $file) {
+            println($file);
+        }
+
+        println('FALLBACK:');
+        foreach ($this->fallback as $file) {
+            println($file);
+        }
     }
 }
 
