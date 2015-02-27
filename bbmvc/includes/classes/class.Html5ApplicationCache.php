b20e0ab863fcc5a990771ac48b41b04e98273fe1
ovidiugabriel@gmail.com <ovidiugabriel@gmail.com@0ceaffdf-74a6-3e93-7e3e-2094549a50af>
2015-02-27 20:30:34 +0000

    added members

--- bbmvc/includes/classes/class.Html5ApplicationCache.php
+++ bbmvc/includes/classes/class.Html5ApplicationCache.php
@@ -1,10 +1,21 @@
 <?php
 
-  /**
-   * HTML5 Application Cache file generator
-   */
-  class Html5ApplicationCache {
-  }
+/**
+ * HTML5 Application Cache file generator
+ */
+class Html5ApplicationCache {
+}
+
+class CacheManifest {
+    // Files listed under this header will be cached after they are downloaded for the first time
+    private $cache_manifest = array();
   
-  class CacheManifest {
-  }
+    // Files listed under this header require a connection to the server, and will never be cached
+    private $network        = array();
+  
+    // Files listed under this header specifies fallback pages if a page is inaccessible
+    private $fallback       = array();
+    
+    public function print() {
+    }
+}
