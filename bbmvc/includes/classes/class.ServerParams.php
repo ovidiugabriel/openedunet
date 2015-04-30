422349fd44866f269b68e03fb28880ecd8c74aaf
ovidiugabriel <ovidiugabriel@gmail.com>
2015-04-30 17:55:59 +0300

    Update class.ServerParams.php

--- bbmvc/includes/classes/class.ServerParams.php
+++ bbmvc/includes/classes/class.ServerParams.php
@@ -1,20 +1,23 @@
 <?php
 
 //
-// |--------------------------|---------------------|---------------------------
-// | Format                   | Type                |
-// |--------------------------|---------------------|---------------------------
-// | REQUEST                  | Map<String, String> | Array<String> not supported
-// | PATH_INFO_JSON           | Map<String, String> | Array<String> also supported
-// | PATH_INFO_SPLIT_COLON    | Array<String>       | Map<String, String> may be supported with separate call??
-// | PATH_INFO_SPLIT_SLASH    | Array<String>       | Map<String, String> may be supported with separate call??
-// | PATH_INFO_SPLIT_ASSOC    | Map<String, String> |
-// | QUERY_STRING_JSON        | Map<String, String> |
-// | QUERY_STRING_SPLIT_COLON | Array<String>       | Map<String, String> may be supported with separate call??
-// | QUERY_STRING_SPLIT_SLASH | Array<String>       | Map<String, String> may be supported with separate call??
-// | PATH_INFO_CSV            | Array<String>       |
-// | PATH_INFO_INI_ASSOC      | Map<String, String> |
-// |--------------------------|---------------------|---------------------------
+// dict = Map<String, String>
+// list = Array<String>
+//
+// |------------------------------|---------------------|
+// | Format                       | Type                |
+// |------------------------------|---------------------|
+// |  1) REQUEST                  | dict                | list not supported yet
+// |  2) QUERY_STRING_SPLIT_COLON | list                | Map<String, String> may be supported with separate call??
+// |  3) QUERY_STRING_SPLIT_SLASH | list                | Map<String, String> may be supported with separate call??
+// |  4) PATH_INFO_SPLIT_SLASH    | list                | Map<String, String> may be supported with separate call??
+// |  5) PATH_INFO_SPLIT_ASSOC    | Map<String, String> |
+// |  6) PATH_INFO_SPLIT_COLON    |        | Map<String, String> may be supported with separate call??
+// |  7) PATH_INFO_JSON           | Map<String, String> | Array<String> also supported
+// | QUERY_STRING_JSON            | Map<String, String> |
+// |  9) PATH_INFO_CSV            | list                |
+// | 10) PATH_INFO_INI_ASSOC      | Map<String, String> |
+// |------------------------------|---------------------|
 //
 if (!function_exists('safe_count')) {
     function safe_count(array $a) { return count($a); }
