2f94896b377f2dc744c11c33b85b37b9b199d393
ovidiugabriel <ovidiugabriel@gmail.com>
2015-05-11 22:03:18 +0300

    Update class.CSSBoxModel.php

--- bbmvc/includes/classes/class.CSSBoxModel.php
+++ bbmvc/includes/classes/class.CSSBoxModel.php
@@ -1,47 +1,133 @@
 <?php 
 
-class CSSBoxModel {
-  private $width;
-  private $height;
+class CssBoxModel implements IBoxModelAccess {
+    protected $width;
+    protected $height;
+    protected $left;
+    protected $top;
 
-  public $left;
-  public $top;
+    // Padding
+    protected $paddingBottom;
+    protected $paddingLeft;
+    protected $paddingRight;
+    protected $paddingTop;
 
-  // Padding
-  public $paddingBottom;
-  public $paddingLeft;
-  public $paddingRight;
-  public $paddingTop;
+    // Border
+    protected $borderBottomWidth;
+    protected $borderLeftWidth;
+    protected $borderRightWidth;
+    protected $borderTopWidth;
 
-  // Border
-  public $borderBottomWidth;
-  public $borderLeftWidth;
-  public $borderRightWidth;
-  public $borderTopWidth;
+    // BorderRadius
+    protected $borderTopLeftRadius;
+    protected $borderTopRightRadius;
+    protected $borderBottomLeftRadius;
+    protected $borderBottomRightRadius;
 
-  // BorderRadius
-  public $borderTopLeftRadius;
-  public $borderTopRightRadius;
-  public $borderBottomLeftRadius;
-  public $borderBottomRightRadius;
-	
-  // Margin
-  public $marginBottom;
-  public $marginLeft;
-  public $marginRight;
-  public $marginTop;
-  
-  public function width($width = null) {
-  	if (null !== $width) {
-  		$this->width = (float) $width;
-  	}
-  	return (float) $this->width;
-  }
-  
-  public function height($height = null) {
-  	if (null !== $height) {
-  		$this->height = (float) $height;
-  	}
-  	return (float) $this->height;
-  }
+    // Margin
+    protected $marginBottom;
+    protected $marginLeft;
+    protected $marginRight;
+    protected $marginTop;
+
+    public function getWidth()
+    {
+        return (float) $this->width;
+    }
+
+    public function getHeight()
+    {
+        return (float) $this->height;
+    }
+
+    public function getLeft()
+    {
+        return (float) $this->left;
+    }
+
+    public function getTop()
+    {
+        return (float) $this->top;
+    }
+
+    public function getPaddingBottom()
+    {
+        return (float) $this->paddingBottom;
+    }
+
+    public function getPaddingLeft()
+    {
+        return (float) $this->paddingBottom;
+    }
+
+    public function getPaddingRight()
+    {
+        return (float) $this->paddingRight;
+    }
+
+    public function getPaddingTop()
+    {
+        return (float) $this->paddingTop;
+    }
+
+    public function getBorderBottomWidth()
+    {
+        return (float) $this->borderBottomWidth;
+    }
+
+    public function getBorderLeftWidth()
+    {
+        return (float) $this->borderLeftWidth;
+    }
+
+    public function getBorderRightWidth()
+    {
+        return (float) $this->borderRightWidth;
+    }
+
+    public function getBorderTopWidth()
+    {
+        return (float) $this->borderTopWidth;
+    }
+
+    public function getBorderTopLeftRadius()
+    {
+        return (float) $this->borderTopLeftRadius;
+    }
+
+
+    public function getBorderTopRightRadius()
+    {
+        return (float) $this->borderTopRightRadius;
+    }
+
+    public function getBorderBottomLeftRadius()
+    {
+        return (float) $this->borderBottomLeftRadius;
+    }
+
+    public function getBorderBottomRightRadius()
+    {
+        return (float) $this->borderBottomRightRadius;
+    }
+
+    public function getMarginBottom()
+    {
+        return (float) $this->marginBottom;
+    }
+
+    public function getMarginLeft()
+    {
+        return (float) $this->marginLeft;
+    }
+
+    public function getMarginRight()
+    {
+        return (float) $this->marginRight;
+    }
+
+    public function getMarginTop()
+    {
+        return (float) $this->marginTop;
+    }
 }
