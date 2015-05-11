7d8c7bfdfe706899565bd978971fb6ad3c722826
ovidiugabriel <ovidiugabriel@gmail.com>
2015-05-11 17:29:16 +0300

    Update class.CSSBoxModel.php

--- bbmvc/includes/classes/class.CSSBoxModel.php
+++ bbmvc/includes/classes/class.CSSBoxModel.php
@@ -1,5 +1,35 @@
 <?php 
 
 class CSSBoxModel {
+  public $width;
+  public $height;
 
+  public $left;
+  public $top;
+
+  // Padding
+  public $paddingBottom;
+  public $paddingLeft;
+  public $paddingRight;
+  public $paddingTop;
+
+  // Border
+  public $borderBottomWidth;
+  public $borderLeftWidth;
+  public $borderRightWidth;
+  public $borderTopWidth;
+
+  // BorderRadius
+  public $borderTopLeftRadius;
+  public $borderTopRightRadius;
+  public $borderBottomLeftRadius;
+  public $borderBottomRightRadius;
+	
+  // borderSpacing??
+
+  // Margin
+  public $marginBottom;
+  public $marginLeft;
+  public $marginRight;
+  public $marginTop;
 }
