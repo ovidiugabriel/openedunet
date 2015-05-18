e06cd9782e028a90e5742b3443ca19567cd7fd88
ovidiugabriel <ovidiugabriel@gmail.com>
2015-05-18 10:35:01 +0300

    Update class.Hyperlink.php

--- bbmvc/includes/classes/class.Hyperlink.php
+++ bbmvc/includes/classes/class.Hyperlink.php
@@ -1,4 +1,12 @@
 <?php
 
 class Hyperlink {
+  public function __construct($href, $text) {
+    $this->href = $href;
+    $this->text = $text;
+  }
+  
+  public function __toString() {
+    return '<a href="'.$this->href.'">'.$this->text.'</a>';
+  }
 }
