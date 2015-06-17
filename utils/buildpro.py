e9697ee3e10f5262bcd0e9cb75c69036e74526db
ovidiugabriel <ovidiugabriel@gmail.com>
2015-06-17 11:44:23 +0300

    Update buildpro.py

--- utils/buildpro.py
+++ utils/buildpro.py
@@ -4,7 +4,7 @@
 # The main idea of this tool is that it can generate tupfiles, makefiles, etc.
 # Or it can pass strings directly to your favourite compiler, gcc, dmc, etc.
 #
- 
+
 #
 # After binary artifact is generated, unit-test is performed, and test report
 # with test coverage is displayed
@@ -35,7 +35,7 @@ def shell_exec(cmd):
 def get_tupfile(deps):
     COMMAND = 0;
     OUTPUT  = 1;
- 
+
     tup_out = ''
     for key in deps:
         tup_out += (': ' + key + ' |> ' + deps[key][COMMAND] + ' |> ' + deps[key][OUTPUT] + "\n")
@@ -48,30 +48,35 @@ if 1 == len(sys.argv):
 env = os.environ
 # TODO: `final_cmd` must be formatted as specified in the .project.yml file.
 # The `final_cmd` may be used to run the compiler directly but also to generate Tupfile
-final_cmd = 'gcc '
+final_cmd = 'gcc'
 project_file = sys.argv[1] + '.project.yml'
 stream = file(project_file, 'r')
 data = yaml.load(stream)
+
+if data == None:
+    print('Error: Invalid project file.')
+    exit(1)
 
 # FIXME: As it is implemented now, it works only with GCC.
 
 # http://scribu.net/blog/python-equivalents-to-phps-foreach.html
 
 # Includes paths
-if data['includes'] != None:
-    # https://wiki.python.org/moin/HandlingExceptions
-    try:
-        include_paths = []
-        for (key, value) in enumerate(data['includes']):
-            value = value.format(**env).replace('$', '')
-            data['includes'][key] = value
-            include_paths.append('-I' + value)
+if 'includes' in data:
+    if data['includes'] != None:
+        # https://wiki.python.org/moin/HandlingExceptions
+        try:
+            include_paths = []
+            for (key, value) in enumerate(data['includes']):
+                value = value.format(**env).replace('$', '')
+                data['includes'][key] = value
+                include_paths.append('-I' + value)
 
-        final_cmd += (' '.join(include_paths) + ' ')
-    except KeyError, ex:
-        print('Key Error: Undefined environment variable ${' + str(ex).strip("'") + '}')
-        exit(1)
-        
+            final_cmd += (' '.join(include_paths) + ' ')
+        except KeyError, ex:
+            print('Key Error: Undefined environment variable ${' + str(ex).strip("'") + '}')
+            exit(1)
+
 defines = {}
 defines['False'] = '0'
 defines['True']  = '1'
@@ -79,10 +84,11 @@ defines['True']  = '1'
 cdefs = []
 for key in defines:
     cdefs.append('-D' + key + '=' + defines[key])
-final_cmd(' ' +  ' '.join(cdefs) + ' ')
+final_cmd += (' ' +  ' '.join(cdefs) + ' ')
 
-for value in data['sources']:
-    final_cmd += value + ' '
+if 'sources' in data:
+    for value in data['sources']:
+        final_cmd += value + ' '
 
 if 'library_paths' in data:
     libpaths = []
@@ -91,18 +97,26 @@ if 'library_paths' in data:
     final_cmd += (' ' + ' '.join(libpaths) + ' ')
 
 #
-# the libs have to go after sources list 
+# the libs have to go after sources list
 # don't ask me why, I don't know, but the order seems to be required
 #
-libraries = []
-for value in data['libraries']:
-    libraries.append('-l' + value)
-final_cmd += (' ' + ' '.join(libraries) + ' ')
-
+if 'libraries' in data:
+    libraries = []
+    for value in data['libraries']:
+        libraries.append('-l' + value)
+    final_cmd += (' ' + ' '.join(libraries) + ' ')
 
 # append artifact name
-output = data['artifact']['name']
-final_cmd += ' -g -o ' + output
+output = 'a.out'
+if 'artifact' in data:
+    output = data['artifact']['name']
+else:
+    if 'artefact' in data:
+        output = data['artefact']['name']
+
+# g flag - Produce debugging information in the operating system's native format
+# GDB can work with this debugging information.
+final_cmd += '-g -o ' + output
 
 print(final_cmd)
 print(shell_exec(final_cmd))
@@ -110,6 +124,8 @@ print(shell_exec(final_cmd))
 if os.path.isfile('./' + output):
   print('### Running ' + output + ' ... ###');
   print(shell_exec('./' + output))
+
+exit(0)
 
 # Scanning dependencies ...
 
@@ -126,10 +142,3 @@ deps = {}
 deps[' '.join(inputs)] = ['gcc -c {input} -o {output}'.format(input=inputs[0], output=output), output]
 
 # print(get_tupfile(deps))
-
-
-
-
-
-
-
