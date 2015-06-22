12b40e159fc844b55582cf132a9992305ef98569
ovidiugabriel <ovidiugabriel@gmail.com>
2015-06-22 22:52:06 +0300

    Update Dispatcher.class.php

--- bbmvc/includes/Dispatcher.class.php
+++ bbmvc/includes/Dispatcher.class.php
@@ -73,7 +73,10 @@ function redirect($params) {
 }
 
 function no_cache() {
-    
+    header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
+    header('Cache-Control: no-store, no-cache, must-revalidate');
+    header('Cache-Control: post-check=0, pre-check=0', false /* replace=false */);
+    header('Pragma: no-cache');
 }
 
 class Dispatcher {
