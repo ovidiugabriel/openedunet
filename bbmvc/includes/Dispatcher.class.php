2c621383b7d2255e879a99057c0b5921b06ee328
ovidiugabriel <ovidiugabriel@gmail.com>
2015-06-22 22:53:11 +0300

    Update Dispatcher.class.php

--- bbmvc/includes/Dispatcher.class.php
+++ bbmvc/includes/Dispatcher.class.php
@@ -74,7 +74,7 @@ function redirect($params) {
 
 // TODO: Consider moving to a `headers` class or something like that.
 function no_cache() {
-    header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
     header('Cache-Control: no-store, no-cache, must-revalidate');
     header('Cache-Control: post-check=0, pre-check=0', false /* replace=false */);
     header('Pragma: no-cache');
