95cbfdbc7a0cec2270b4145becd0ac8ca76ba6ca
ovidiugabriel <ovidiugabriel@gmail.com>
2015-06-22 12:06:34 +0300

    Update config.php

--- bbmvc/includes/smarty_extended/config.php
+++ bbmvc/includes/smarty_extended/config.php
@@ -1,7 +1,13 @@
 <?php
 
+/*
+ * The type must be one of:
+ *      - Smarty::PLUGIN_FUNCTION
+ *      - Smarty::PLUGIN_BLOCK
+ *      - Smarty::PLUGIN_MODIFIER
+ */
 return array(
-    array('type' => 'block',    'name' => 'a'),
-    array('type' => 'function', 'name' => 'lprintf'),
-    array('type' => 'function', 'name' => 'url')
+    array('type' => Smarty::PLUGIN_BLOCK,    'name' => 'a'),
+    array('type' => Smarty::PLUGIN_FUNCTION, 'name' => 'lprintf'),
+    array('type' => Smarty::PLUGIN_FUNCTION, 'name' => 'url')
 );
