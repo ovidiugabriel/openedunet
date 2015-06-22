813bba8571eb7f5a58141bc25a5ca987a17a8e40
ovidiugabriel <ovidiugabriel@gmail.com>
2015-06-22 22:25:18 +0300

    Update Dispatcher.class.php

--- bbmvc/includes/Dispatcher.class.php
+++ bbmvc/includes/Dispatcher.class.php
@@ -44,6 +44,7 @@
 /*                                                                           */
 /* Date         Name    Reason                                               */
 /* ------------------------------------------------------------------------- */
+/* 22.06.2015           Replaced dispatcher function with Dispatcher class   */
 /* 08.06.2015           Moved require_object(), etc to class_loader.php      */
 /* 13.06.2014           Added InvalidArgumentException to import()           */
 /* 11.03.2014           Added require_object(), require_class()              */
