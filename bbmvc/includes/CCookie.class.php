db31aa2b4adce5b45041f71fbba4388c11cd40bf
ovidiugabriel <ovidiugabriel@gmail.com>
2015-07-09 14:43:51 +0300

    Update CCookie.class.php

--- bbmvc/includes/CCookie.class.php
+++ bbmvc/includes/CCookie.class.php
@@ -5,4 +5,10 @@
  * https://www.softintegration.com/docs/ch/cgi/chcgi/CCookie.html
  */
 class CCookie {
+    public function addPort($portNum) {}
+    public function getComment(){}
+    public function getCommentURL(){}
+    public function getDiscard(){}
+    public function getDomain(){}
+
 }
