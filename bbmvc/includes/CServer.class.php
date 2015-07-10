f42ab91f8df2e59c0f83476c6f2c63d985e9b740
ovidiugabriel <ovidiugabriel@gmail.com>
2015-07-10 11:41:27 +0300

    Update CServer.class.php

--- bbmvc/includes/CServer.class.php
+++ bbmvc/includes/CServer.class.php
@@ -5,4 +5,25 @@
  * https://www.softintegration.com/docs/ch/cgi/chcgi/CServer.html
  */
 class CServer {
+    /** 
+     * Applies HTML encoding to the specified string.
+     * 
+     * @proto public HTMLEncode(in:String):String  
+     */
+    public function HTMLEncode($in) {}
+
+    /** 
+     * Maps the specified relative or virtual path to the corresponding physical directory on the server.
+     * 
+     * @proto public mapPath(in:String):String 
+     */
+    public function mapPath($in) {}
+
+    /**
+     * Applies URL encoding rules, including escape characters, to the specified string.
+     * 
+     * @proto public URLEncode(in:String):String 
+     */
+    public function URLEncode($in) {}
+
 }
