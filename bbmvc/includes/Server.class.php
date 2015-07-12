10b04ee20cde3f40f5ca6c47b8541c45b695c062
ovidiugabriel <ovidiugabriel@gmail.com>
2015-07-12 22:32:21 +0300

    Update and rename CServer.class.php to Server.class.php

--- bbmvc/includes/Server.class.php
+++ bbmvc/includes/Server.class.php
@@ -4,7 +4,7 @@
  * This is a port of the class described at:
  * https://www.softintegration.com/docs/ch/cgi/chcgi/CServer.html
  */
-class CServer {
+class Server {
     /** 
      * Applies HTML encoding to the specified string.
      * 
