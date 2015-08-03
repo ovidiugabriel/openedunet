f8bf28dffae5300a85072e8ef3ca87ce220c888e
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-03 14:00:12 +0300

    Update class.Result.php

--- bbmvc/includes/classes/sql/class.Result.php
+++ bbmvc/includes/classes/sql/class.Result.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace sql;
+declare_namespace('sql');
 
 /** Abstract interface for accessing data from specific SQL databases */
-abstract class Result {
+abstract class sql_Result {
 }
 
-class Result_BindingSyntax {
-  const NamedBinding      = 0;
-  const PositionalBinding = 1;
+class sql_Result_BindingSyntax {
+    const NamedBinding      = 0;
+    const PositionalBinding = 1;
 }
 
-class Result_VirtualHookOperation {
-  const BatchOperation        = 0;
-  const DetachFromResultSet   = 1;
-  const NextResult            = 2;
-  const SetNumericalPrecision = 3;
+class sql_Result_VirtualHookOperation {
+    const BatchOperation        = 0;
+    const DetachFromResultSet   = 1;
+    const NextResult            = 2;
+    const SetNumericalPrecision = 3;
 }
