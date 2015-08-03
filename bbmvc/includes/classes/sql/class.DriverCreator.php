784f51f2bca12ff415c59db2d365847449e1006f
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-03 14:23:11 +0300

    Update class.DriverCreator.php

--- bbmvc/includes/classes/sql/class.DriverCreator.php
+++ bbmvc/includes/classes/sql/class.DriverCreator.php
@@ -1,6 +1,6 @@
 <?php
 
-// namespace sql;
+declare_namespace('sql');
 
 /** Template class that provides a SQL driver factory for a specific driver type */
 class sql_DriverCreator extends sql_DriverCreatorBase {
