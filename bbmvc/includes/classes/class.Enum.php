e1b2aa71a76d4e4a0befea7136e3ba859155a6df
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-03 14:30:49 +0300

    Replaced tabs with spaces

--- bbmvc/includes/classes/class.Enum.php
+++ bbmvc/includes/classes/class.Enum.php
@@ -1,8 +1,8 @@
 <?php
 
 class Enum {
-	static public function values($class_name) {
-		$reflection = new ReflectionClass($class_name);
-		return $reflection->getConstants();
-	}
+    static public function values($class_name) {
+        $reflection = new ReflectionClass($class_name);
+        return $reflection->getConstants();
+    }
 }
