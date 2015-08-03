9778c3e1ed6898dbae1acd0dbf363693e1792e55
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-03 13:58:49 +0300

    Update class.TableModel.php

--- bbmvc/includes/classes/sql/class.TableModel.php
+++ bbmvc/includes/classes/sql/class.TableModel.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace sql;
+declare_namespace('sql');
 
 /** 
  * Editable data model for a single database table 
  */
-class TableModel extends QueryModel {
+class sql_TableModel extends QueryModel {
 }
 
-class TableModel_EditStrategy {
+class sql_TableModel_EditStrategy {
 }
