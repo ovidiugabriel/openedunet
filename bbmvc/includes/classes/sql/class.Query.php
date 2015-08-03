6f0fb17cbc436b4cfa64cb4c74154958915716e2
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-03 13:42:54 +0300

    Update class.Query.php

--- bbmvc/includes/classes/sql/class.Query.php
+++ bbmvc/includes/classes/sql/class.Query.php
@@ -1,23 +1,22 @@
 <?php
 
-namespace sql;
+// namespace sql;
 
 /** 
  * Means of executing and manipulating SQL statements 
  */
-class Query {
+class sql_Query {
 }
 
-class Query_BatchExecutionMode {
-  const ValuesAsColumns = 0;
-  const ValuesAsRows    = 1;
+class sql_Query_BatchExecutionMode {
+    const ValuesAsColumns = 0;
+    const ValuesAsRows    = 1;
 
-  static public function values()
-  {
-    return array(
-      'ValuesAsColumns' => self::ValuesAsColumns,
-      'ValuesAsRows'    => self::ValuesAsRows,
-    );
-  }
-
+    static public function values()
+    {
+        return array(
+            'ValuesAsColumns' => self::ValuesAsColumns,
+            'ValuesAsRows'    => self::ValuesAsRows,
+        );
+    }
 }
