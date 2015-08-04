930cb31e07184faeb3cecd995fb1940091ae4b43
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-04 19:43:12 +0300

    Update constdef.php

--- bbmvc/includes/constdef.php
+++ bbmvc/includes/constdef.php
@@ -37,4 +37,33 @@ define('_DEBUG_OFF', 0);
 define('_DEBUG_BROWSER', 1);
 define('_DEBUG_LOG', 2);
 
+/**
+ * @param string $def
+ * @param mixed $yes
+ * @param mixed $no
+ * @return mixed
+ */
+function ifdef($def, $yes, $no, $fn = null) {
+    $val = defined($def) ? $yes : $no;
+    if (null !== $fn) {
+        $val = $fn($val);
+    }
+    return $val;
+}
+
+/** 
+ * @param mixed $s1
+ * @param mixed $s2
+ * @param mixed $eq
+ * @param mixed $neq
+ */
+function ifeqelse($s1, $s2, $eq, $neq, $fn = null) {
+    $val = ($s1 === $s2) ? $eq : $neq;
+    if (null !== $fn) {
+        $val = $fn($val);
+    }
+    return $val;
+}
+
+
 // EOF
