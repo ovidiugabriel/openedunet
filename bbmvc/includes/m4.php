fa7c139786b918e3afbe180e76ae169c81452952
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-05 12:44:16 +0300

    Update m4.php

--- bbmvc/includes/m4.php
+++ bbmvc/includes/m4.php
@@ -1,2 +1,29 @@
 <?php
 
+/**
+ * @param string $def
+ * @param mixed $yes
+ * @param mixed $no
+ * @return mixed
+ */
+function ifdef($def, $yes, $no, $fn = null) {
+    $val = defined($def) ? $yes : $no;
+    if (null !== $fn) {
+        $val = $fn($val);
+    }
+    return $val;
+}
+
+/** 
+ * @param mixed $s1
+ * @param mixed $s2
+ * @param mixed $eq
+ * @param mixed $neq
+ */
+function ifeqelse($s1, $s2, $eq, $neq, $fn = null) {
+    $val = ($s1 == $s2) ? $eq : $neq;
+    if (null !== $fn) {
+        $val = $fn($val);
+    }
+    return $val;
+}
