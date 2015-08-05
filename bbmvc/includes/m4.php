dc05a209ec457d22822d1016e3683bbe969a14ef
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-05 12:55:02 +0300

    Update m4.php

--- bbmvc/includes/m4.php
+++ bbmvc/includes/m4.php
@@ -1,17 +1,42 @@
 <?php
 
+//
+// This function is so common that I expect it to be already defined somewhere
+// But if it is not, let's define it here.
+//
+if (!function_exists('identity')) {
+    function identity($x) {
+        return $x;
+    }
+}
+
 /**
+ *  When you feel that doing this:
+ * 
+ *      if (defined('CONSTANT_NAME')) {
+ *          $val = 'yes';
+ *      } else {
+ *          $val = 'no';
+ *      }
+ * 
+ * Has no added value, and you understand that this is nothing but a 
+ * basic boolean expression:
+ * 
+ *      $val = defined('CONSTANT_NAME') ? 'yes' : 'no';
+ * 
+ * But if for some deceptive reason you want to avoid ternary operator, 
+ * (or you are not allowed to use it), then just do it this way:
+ * 
+ *     $val = ifdef('CONSTANT_NAME', 'yes', 'no');
+ * 
  * @param string $def
  * @param mixed $yes
  * @param mixed $no
+ * @param callable $fn
  * @return mixed
  */
-function ifdef($def, $yes, $no, $fn = null) {
-    $val = defined($def) ? $yes : $no;
-    if (null !== $fn) {
-        $val = $fn($val);
-    }
-    return $val;
+function ifdef($def, $yes, $no, $fn = 'identity') {
+    return $fn(defined($def) ? $yes : $no);
 }
 
 /** 
@@ -19,11 +44,8 @@ function ifdef($def, $yes, $no, $fn = null) {
  * @param mixed $s2
  * @param mixed $eq
  * @param mixed $neq
+ * @param callabke $fn
  */
-function ifeqelse($s1, $s2, $eq, $neq, $fn = null) {
-    $val = ($s1 == $s2) ? $eq : $neq;
-    if (null !== $fn) {
-        $val = $fn($val);
-    }
-    return $val;
+function ifeqelse($s1, $s2, $eq, $neq, $fn = 'identity') {
+    return $fn(($s1 == $s2) ? $eq : $neq);
 }
