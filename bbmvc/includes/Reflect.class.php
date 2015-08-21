25f29d87cf99d852a68a307554d3af5854626781
ovidiugabriel <ovidiugabriel@gmail.com>
2015-08-21 17:38:53 +0300

    Update Reflect.class.php

--- bbmvc/includes/Reflect.class.php
+++ bbmvc/includes/Reflect.class.php
@@ -1,24 +1,35 @@
 <?php
 
 class Reflect {
+    /** 
+     * @param string|object $class - Either a string containing the name of the class to reflect, or an object.
+     * @return ReflectionClass
+     * @proto static public function getReflectionClass(s_class:Dynamic):php.ReflectionClass
+     */
     public static function getReflectionClass($class) {
         static $reflections = array();
-        if (!isset($reflections[$class])) {
-            $reflections[$class] = new ReflectionClass($class);
+        $class_name = self::getClassName($class);
+        if (!isset($reflections[$class_name])) {
+            $reflections[$class_name] = new ReflectionClass($class);
         }
-        return $reflections[$class];
+        return $reflections[$class_name];
     }
     
     public static function getReflectionMethod($class, $method) {
         static $reflections = array();
-        if (!isset($reflections[$class][$method])) {
+        $class_name = self::getClassName($class);
+        if (!isset($reflections[$class_name][$method])) {
             $reflection_class = self::getReflectionClass($class);
-            $reflections[$class][$method] = $reflection_class->getMethod($method);
+            $reflections[$class_name][$method] = $reflection_class->getMethod($method);
         }
-        return $reflections[$class][$method];
+        return $reflections[$class_name][$method];
     }
     
     public static function invoke($object, $method, $args) {
         return call_user_func_array ( array ($object, $method ), $args );
+    }
+    
+    private static function getClassName($class) {
+        return is_string($class) ? $class : get_class($class);
     }
 }
