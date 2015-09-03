36b8b113abeea37ce8eae9246da0842708933bba
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-03 14:07:52 +0300

    Update Results.class.php

--- bbmvc/includes/classes/play/mvc/Results.class.php
+++ bbmvc/includes/classes/play/mvc/Results.class.php
@@ -11,83 +11,84 @@ class play_mvc_Results {
 	 * @proto static public badRequest(?content:String):play.mvc.results.Status 
 	 */
 	static function badRequest($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::BAD_REQUEST, $content);
+		return self::status(play_mvc_http_Status::BAD_REQUEST, $content);
 	}
 	
 	/** 
 	 * @proto static public created(?content:String):play.mvc.results.Status
 	 */
 	static function created($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::CREATED, $content);
+		return self::status(play_mvc_http_Status::CREATED, $content);
 	}
 	
 	/** 
 	 * @proto static public forbidden(?content:String):play.mvc.results.Status
 	 */
 	static function forbidden($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::FORBIDDEN, $content);
+		return self::status(play_mvc_http_Status::FORBIDDEN, $content);
 	}
 	
 	/** 
 	 * @proto static public found(url:String):play.mvc.results.Status
 	 */
 	static function found($url) {
-		return new play_mvc_results_Redirect(play_mvc_results_Redirect::FOUND, $url);
+		return self::status(play_mvc_http_Status::FOUND, $url);
 	}
 	
 	/** 
 	 * @proto static public internalServerError(?content:String):play.mvc.results.Status
 	 */
 	static function internalServerError($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::INTERNAL_SERVER_ERROR, $content);
+		return self::status(play_mvc_http_Status::INTERNAL_SERVER_ERROR, $content);
 	}
 	
 	/** 
 	 * @proto static public movedPermanently(url:String):play.mvc.results.Redirect
 	 */
 	static function movedPermanently($url) {
-		return new play_mvc_results_Redirect(play_mvc_results_Redirect::MOVED_PERMANENTLY, $url);
+		return new play_mvc_results_Redirect(play_mvc_http_Status::MOVED_PERMANENTLY, $url);
 	}
 	
 	/** 
 	 * @proto static public noContent():play.mvc.results.Status
 	 */
 	static function noContent() {
-		return new play_mvc_results_Status(play_mvc_results_Status::NO_CONTENT, null);
+		return self::status(play_mvc_http_Status::NO_CONTENT, null);
 	}
 	
 	/** 
 	 * @proto static public notFound(?content:String):play.mvc.results.Status
 	 */
 	static function notFound($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::NOT_FOUND, null);
+		return self::status(play_mvc_http_Status::NOT_FOUND, null);
 	}
 	
 	/** 
 	 * @proto static public ok(?content:String):play.mvc.results.Status
 	 */
 	static function ok($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::OK, null);
+		return self::status(play_mvc_http_Status::OK, null);
 	}
 	
 	/** 
 	 * @proto static public redirect(url:String):play.mvc.results.Redirect
 	 */
 	static function redirect($url) {
-		return new play_mvc_results_Redirect(play_mvc_results_Redirect::FOUND, $url);
+		return new play_mvc_results_Redirect(play_mvc_http_Status::FOUND, $url);
 	}
 	
 	/** 
 	 * @proto static public seeOther(url:String):play.mvc.results.Redirect
 	 */
 	static function seeOther($url) {
-		return new play_mvc_results_Redirect(play_mvc_results_Redirect::SEE_OTHER, $url);
+		return new play_mvc_results_Redirect(play_mvc_http_Status::SEE_OTHER, $url);
 	}
 	
 	/** 
 	 * @proto static public status(status:Int, content:String):play.mvc.results.Status
 	 */
 	static function status($status, $content = null) {
+		// KEEP
 		return new play_mvc_results_Status($status, $content);
 	}
 	
@@ -95,14 +96,14 @@ class play_mvc_Results {
 	 * @proto static public temporaryRedirect(url:String):play.mvc.results.Redirect
 	 */
 	static function temporaryRedirect($url) {
-		return new play_mvc_results_Redirect(play_mvc_results_Redirect::TEMPORARY_REDIRECT, $url);
+		return new play_mvc_results_Redirect(play_mvc_http_Status::TEMPORARY_REDIRECT, $url);
 	}
 	
 	/**
 	 * @proto static public unauthorized(?content:String):play.mvc.results.Status
 	 */
 	static function unauthorized($content = null) {
-		return new play_mvc_results_Status(play_mvc_results_Status::UNAUTHORIZED, null);
+		return self::status(play_mvc_http_Status::UNAUTHORIZED, null);
 	}
 	function __toString() { return 'play.mvc.Results'; }
 }
