933179d06f0e80f7b48a840b6da2475066c21c46
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-03 12:07:25 +0300

    Update Result.interface.php

--- bbmvc/includes/classes/play/mvc/Result.interface.php
+++ bbmvc/includes/classes/play/mvc/Result.interface.php
@@ -1,14 +1,74 @@
 <?php
 
 interface play_mvc_Result {
+    /** 
+     * Extracts the Charset of this Result value.
+     * 
+     * @proto public charset():String
+     */
     public function charset();
+    
+    /** 
+     * Extracts the Content-Type of this Result value.
+     * 
+     * @proto public contentType():String
+     */
     public function contentType();
+    
+    /** 
+     * Extracts a Cookie value from this Result value
+     * 
+     * @proto public cookie(name:String):play.mvc.http.Cookie
+     */
     public function cookie($name);
+    
+    /** 
+     * Extracts the Cookies (an iterator) from this result value.
+     * 
+     * @proto public cookies():play.mvc.http.Cookies
+     */
     public function cookies();
+    
+    /** 
+     * Extracts the Flash values of this Result value.
+     * 
+     * @proto public flash():play.mvc.http.Flash
+     */
     public function flash();
+    
+    /**
+     * Extracts an Header value of this Result value.
+     * 
+     * @proto public header(header:String):String
+     */
     public function header($header);
+    
+    /** 
+     * Extracts all Headers of this Result value.
+     * 
+     * @return array
+     * @proto public headers():Dynamic
+     */
     public function headers();
+    
+    /** 
+     * Extracts the Location header of this Result value if this Result is a Redirect.
+     * 
+     * @proto public redirectLocation():String
+     */
     public function redirectLocation();
+
+    /** 
+     * Extracts the Session of this Result value.
+     * 
+     * @proto public session():play.mvc.http.Session
+     */
     public function session();
+
+    /**
+     * Extracts the Status code of this Result value.
+     * 
+     * @proto public status():Int
+     */
     public function status();
 }
