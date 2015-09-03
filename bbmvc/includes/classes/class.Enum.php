d01a105db8723b9973bbaf639e097c7dbedf56cd
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-03 14:16:31 +0300

    Update class.Enum.php

--- bbmvc/includes/classes/class.Enum.php
+++ bbmvc/includes/classes/class.Enum.php
@@ -2,9 +2,7 @@
 
 class Enum {
     public function values() {
-        $class_name = get_class($this);
-        $reflection = new ReflectionClass($class_name);
-        return $reflection->getConstants();
+        return Reflect::getReflectionClass($this)->getConstants();
     }
     
     public function valueOf($name) {
