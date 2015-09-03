5501be2b57e17f3659acdbdf41def83070389c93
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-03 12:24:06 +0300

    Update Redirect.class.php

--- bbmvc/includes/classes/play/mvc/results/Redirect.class.php
+++ bbmvc/includes/classes/play/mvc/results/Redirect.class.php
@@ -6,5 +6,13 @@ class play_mvc_results_Redirect implements play_mvc_Result{
 	const MOVED_PERMANENTLY = 301;
 	const SEE_OTHER = 303;
 	const TEMPORARY_REDIRECT = 307;
+	
+	/** 
+	 * @proto public new(status:Int, url:String)
+	 */
+	public function __construct($status, $url) {
+		
+	}
+	
 	function __toString() { return 'play.mvc.results.Redirect'; }
 }
