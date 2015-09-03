4d9ae249c744a20fa04a5d7af9faae4cc8f9b276
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-03 13:31:21 +0300

    Create HeaderNames.class.php

---
+++ bbmvc/includes/classes/play/mvc/http/HeaderNames.class.php
@@ -0,0 +1,74 @@
+<?php
+
+/* 
+ * @uuid {abd7327f-d75e-467c-8566-8a4a554c4016}
+ */
+class HeaderNames {
+    const ACCEPT = "Accept";
+    const ACCEPT_CHARSET = "Accept-Charset";
+    const ACCEPT_ENCODING = "Accept-Encoding";
+    const ACCEPT_LANGUAGE = "Accept-Language";
+    const ACCEPT_RANGES = "Accept-Ranges";
+    const ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    const ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    const ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    const ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    const ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    const ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    const ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    const ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+    const AGE = "Age";
+    const ALLOW = "Allow";
+    const AUTHORIZATION = "Authorization";
+    const CACHE_CONTROL = "Cache-Control";
+    const CONNECTION = "Connection";
+    const CONTENT_DISPOSITION = "Content-Disposition";
+    const CONTENT_ENCODING = "Content-Encoding";
+    const CONTENT_LANGUAGE = "Content-Language";
+    const CONTENT_LENGTH = "Content-Length";
+    const CONTENT_LOCATION = "Content-Location";
+    const CONTENT_MD5 = "Content-MD5";
+    const CONTENT_RANGE = "Content-Range";
+    const CONTENT_TRANSFER_ENCODING = "Content-Transfer-Encoding";
+    const CONTENT_TYPE = "Content-Type";
+    const COOKIE = "Cookie";
+    const DATE = "Date";
+    const ETAG = "ETag";
+    const EXPECT = "Expect";
+    const EXPIRES = "Expires";
+    const FORWARDED = "Forwarded";
+    const FROM = "From";
+    const HOST = "Host";
+    const IF_MATCH = "If-Match";
+    const IF_MODIFIED_SINCE = "If-Modified-Since";
+    const IF_NONE_MATCH = "If-None-Match";
+    const IF_RANGE = "If-Range";
+    const IF_UNMODIFIED_SINCE = "If-Unmodified-Since";
+    const LAST_MODIFIED = "Last-Modified";
+    const LOCATION = "Location";
+    const MAX_FORWARDS = "Max-Forwards";
+    const ORIGIN = "Origin";
+    const PRAGMA = "Pragma";
+    const PROXY_AUTHENTICATE = "Proxy-Authenticate";
+    const PROXY_AUTHORIZATION = "Proxy-Authorization";
+    const RANGE = "Range";
+    const REFERER = "Referer";
+    const RETRY_AFTER = "Retry-After";
+    const SERVER = "Server";
+    const SET_COOKIE = "Set-Cookie";
+    const SET_COOKIE2 = "Set-Cookie2";
+    const TE = "Te";
+    const TRAILER = "Trailer";
+    const TRANSFER_ENCODING = "Transfer-Encoding";
+    const UPGRADE = "Upgrade";
+    const USER_AGENT = "User-Agent";
+    const VARY = "Vary";
+    const VIA = "Via";
+    const WARNING = "Warning";
+    const WWW_AUTHENTICATE = "WWW-Authenticate";
+    const X_FORWARDED_FOR = "X-Forwarded-For";
+    const X_FORWARDED_HOST = "X-Forwarded-Host";
+    const X_FORWARDED_PORT = "X-Forwarded-Port";
+    const X_FORWARDED_PROTO = "X-Forwarded-Proto";
+    const X_REQUESTED_WITH = "X-Requested-With";
+}
