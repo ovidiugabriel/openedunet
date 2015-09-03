e43f44b80e6b351038c9420265391d69e3e496d4
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-03 11:59:41 +0300

    Update Result.interface.php

--- bbmvc/includes/classes/play/mvc/Result.interface.php
+++ bbmvc/includes/classes/play/mvc/Result.interface.php
@@ -1,4 +1,14 @@
 <?php
 
 interface play_mvc_Result {
+    public function charset();
+    public function contentType();
+    public function cookie($name);
+    public function cookies();
+    public function flash();
+    public function header($header);
+    public function headers();
+    public function redirectLocation();
+    public function session();
+    public function status();
 }
