cae8687b969e36b66fc8a58a30169e4a55ed37b0
ovidiugabriel <ovidiugabriel@gmail.com>
2015-09-23 15:44:55 +0300

    Create Config.class.php

---
+++ bbmvc/includes/classes/Config.class.php
@@ -0,0 +1,21 @@
+<?php 
+
+/** 
+ * Very similar to: http://fuelphp.com/docs/classes/config.html
+ *
+ * ------------------------------------------------------------
+ * Formats:
+ *    - PHP format
+ *    - INI format
+ *    - YAML format
+ *    - JSON format (and as subset of YAML)
+ *
+ * Source:
+ *    - Filesystem
+ *    - Databases:
+ *        - memcached
+ *        - SQL database
+ * ------------------------------------------------------------
+ */
+class Config {
+}
