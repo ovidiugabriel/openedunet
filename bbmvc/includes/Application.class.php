e258a9fcd45ab07e1cefd460338a1c4a7d45dc1d
ovidiugabriel <ovidiugabriel@gmail.com>
2015-10-08 17:45:23 +0300

    Update Application.class.php

--- bbmvc/includes/Application.class.php
+++ bbmvc/includes/Application.class.php
@@ -1,6 +1,45 @@
 <?php
 
+/** 
+ * @see http://silex.sensiolabs.org/api/Silex/Application.html
+ */
 class Application {
+    // 
+    // This makes use of the Router to match the patterns
+    //
+    
     public function get($pattern, $to = null) {
+
+    }
+
+    public function post($pattern, $to = null) {
+
+    }
+
+    public function put($pattern, $to = null) {
+
+    }
+
+    public function delete($pattern, $to = null) {
+        
+    }
+
+    public function options($pattern, $to = null) {
+        
+    }
+    public function patch($pattern, $to = null) {
+        
+    }
+    
+    public function redirect($url, $status = 302) {
+     	    
+    }
+
+    public function json($data = array(), $status = 200, array $headers = array()) {
+        
+    }
+
+    public function run(Request $request = null) {
+        
     }
 }
