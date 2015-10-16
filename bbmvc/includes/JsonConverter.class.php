79f2b9cdaeff513302407ec267666876d306c664
ovidiugabriel <ovidiugabriel@gmail.com>
2015-10-16 10:31:31 +0300

    Create JsonConverter.class.php

---
+++ bbmvc/includes/JsonConverter.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class JsonConverter {
+    private $queue = array();
+
+    /**
+     * @param object|array $input
+     * @return string
+     * @proto static public convert(input:Dynamic):String
+     */
+    static public function convert($input) {
+    }
+    
+    /** 
+     * @param object $input
+     * @return void
+     */
+    private function enqueueConvert(&$input) {
+    }
+    
+    /** 
+     * @param object $input
+     * @return void
+     */
+    public function convertObjectToArray(&$input) {
+    }
+}
