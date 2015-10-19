724cc77270858a7e7ba791fd06d00b0595c17dc5
ovidiugabriel <ovidiugabriel@gmail.com>
2015-10-20 00:40:53 +0300

    Synchronized open source implementation

--- bbmvc/includes/classes/Database.class.php
+++ bbmvc/includes/classes/Database.class.php
@@ -1,9 +1,126 @@
 <?php
 
+/* ************************************************************************* */
+/*                                                                           */
+/*  Title:       class.Database.php                                          */
+/*  Purpose:     Database Class                                              */
+/*                                                                           */
+/*  Created on:  16.07.2013 at 10:44:28                                      */
+/*  Email:       ovidiugabriel@gmail.com                                     */
+/*  Copyright:   (C) 2013 ICE Control srl. All Rights Reserved.              */
+/*                                                                           */
+/*  $Id$                                                                     */
+/*                                                                           */
+/* ************************************************************************* */
+
+/*
+ * Copyright (c) 2015, ICE Control srl
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* History (Start).                                                          */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*                                                                           */
+/* Date         Name    Reason                                               */
+/* ------------------------------------------------------------------------- */
+/* 20.10.2015           Synchronized open source implementation              */
+/* 16.07.2013           Added begin(), commit() and rollback()               */
+/*                          as transaction processing functions              */
+/*                      Capitals in update()                                 */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* History (END).                                                            */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 /**
  * @access public
  */
-class Database extends mysqli {
+interface IDatabase {
+    /**
+     *
+     * @param  string $query
+     * @return integer
+     */
+    public function query($query);
+
+    /**
+     *
+     * @param  integer $res_num
+     * @return array
+     */
+    public function nextRow($res_num);
+
+    /**
+     *
+     * @param  string $table
+     * @param  array $a_values
+     * @return integer
+     */
+    public function insert($table, array $a_values);
+
+    /**
+     *
+     * @param  string $table
+     * @param  array $a_values
+     * @param  string $where
+     * @return integer
+     */
+    public function update($table, array $a_values, $where);
+
+    /**
+     * 
+     * @param integer $flags
+     * @param string $name
+     * @return boolean
+     */
+    public function begin_transaction($flags = 0, $name = null);
+    
+    /**
+     * 
+     * @param integer $flags
+     * @param string $name
+     * @return boolean
+     */
+    public function commit($flags = 0, $name = null);
+    
+    /**
+     * 
+     * @param integer $flags
+     * @param string $name
+     * @return boolean
+     */
+    public function rollback($flags = 0, $name = null);
+}
+
+/**
+ * @access public
+ */
+class Database extends mysqli implements IDatabase {
 
     /**
      * @var Database
@@ -11,30 +128,74 @@ class Database extends mysqli {
     private static $instance;
 
     /**
-     * @var
+     *
+     * @var array
      */
-    private static $res;
-
-    /**
-     * @var
-     */
-    private static $res_count;
-
-    /**
-     */
-    private static $history;
+    private static $res = array();
 
     /**
      *
-     * @global array $_websites 
+     * @var integer
      */
-    private function __construct() {
-        global $_websites;
+    private static $res_count = 0;
+    // TODO: Remove res_count since you can count number of elements in self::$res
 
-        parent::mysqli(_DB_HOST, _DB_USER, _DB_PASS, _DB_NAME);
-        self::$history = array();
-        self::$res = array(); //we create an array of resources
-        self::$res_count = 0;
+    /**
+     * @var array
+     */
+    private $history = array();
+
+    /**
+     *
+     * @var string
+     */
+    private $db_host = '';
+
+    /**
+     *
+     * @var string
+     */
+    private $db_name = '';
+
+    /**
+     *
+     * @var string
+     */
+    public $last_query = '';
+
+    /**
+     * Singleton. Private constructor.
+     */
+    private function __construct() {}
+
+    /**
+     *
+     * @param string $username
+     * @param string $password
+     */
+    public function open($username = null, $password = null) {
+        if ((null == $username) && (null == $password)) {
+            parent::mysqli(_DB_HOST, _DB_USER, _DB_PASS, _DB_NAME);
+        }
+        parent::mysqli($this->db_host, $username, $password, $this->db_name);
+    }
+
+    /**
+     *
+     * @param string $name
+     * @return void
+     */
+    public function setDatabaseName($name) {
+        $this->db_name = $name;
+    }
+
+    /**
+     *
+     * @param string $hostname
+     * @return void
+     */
+    public function setHostName($hostname) {
+        $this->db_host = $hostname;
     }
 
     /*
@@ -43,89 +204,142 @@ class Database extends mysqli {
             $resource[$res]->free_result();
         }
     }
-     * 
+     *
      */
 
     /**
      *
-     * @return Database 
+     * @param string $driver
+     * @param array $config
+     * @param type $config_name
+     * @return Database
      */
-    public static function getInstance() {
-        if (empty(self::$instance)) {
-            self::$instance = new Database();
+    static public function factory($driver, array $config = array(), $config_name = 'default') {
+        switch ($driver) {
+            /*
+            case "mssql_odbc":
+                require_once dirname(__FILE__) . "/class.MicrosoftSQL.php";
+                return MicrosoftSQL::getInstance($config, $config_name);
+*/
+            case 'mysqli':
+                return Database::getInstance($config, $config_name);
+
+            default:
+                # code...
+                break;
+        }
+    }
+
+    /**
+     * Creates a connection to a mysql database using the given configuration
+     *
+     * @param array $config
+     * @param string $config_name
+     * @return Database
+     */
+    static public function getInstance(array $config = null, $config_name = 'default') {
+        if (!isset(self::$instance[$config_name])) {
+            $class = __CLASS__;
+            self::$instance[$config_name] = new $class();
+
+            if (null != $config) {
+                self::$instance[$config_name]->setHostName($config['hostname']);
+                self::$instance[$config_name]->setDatabaseName($config['database']);
+                self::$instance[$config_name]->open($config['username'], $config['password']);
+            } else {
+                self::$instance[$config_name]->open();
+            }
         }
 
-        return self::$instance;
+        return self::$instance[$config_name];
+    }
+
+    /**
+     * @param mixed $res
+     * @return integer
+     */
+    static private function push_resource($res) {
+        $res_num = self::$res_count++;
+
+        $num_rows = 0;
+
+        if (is_object($res)) {
+            $num_rows = mysqli_num_rows($res); // better for PHP 5.2 than $res->num_rows
+        }
+        self::$res[$res_num] = array('res' => $res, 'num_rows' => $num_rows);
+
+        return (int) $res_num;
     }
 
     /**
      *
-     * @param type $res
-     * @return type 
+     * @return string
      */
-    private static function push_resource($res) {
-        $res_num = self::$res_count++;
-
-        self::$res[$res_num] = array("res" => $res, "num_rows" => $res->num_rows);
-
-        return $res_num;
+    public function last_query() {
+        return $this->last_query;
     }
 
     /**
      * @override
-     * @param type $query
-     * @return type 
+     * @param string $query
+     * @return integer
      */
     public function query($query) {
-        array_push(self::$history, $query);
+        $this->history[] = $query;
+        $this->last_query = $query;
 
-        $res_num = self::push_resource(parent::query($query));
+        $res = parent::query($query);
+        if ((null == $res) && ($this->errno != 0)) {
+            throw new Exception("Mysql Error: $this->error<br />Query: $query", $this->errno);
+        }
+        $res_num = (int) self::push_resource($res);
 
         if ($this->errno != 0) {
-            throw new Exception("Mysql Error: $this->error<br />Query: $query");
+            throw new Exception("Mysql Error: $this->error<br />Query: $query", $this->errno);
         }
 
-        if (strpos($query, " SQL_CALC_FOUND_ROWS ")) {
-
-            $res_tmp = $this->query("select FOUND_ROWS() as found_rows");
+        if (strpos($query, ' SQL_CALC_FOUND_ROWS ')) {
+            $res_tmp = $this->query('SELECT FOUND_ROWS() AS found_rows');
             $row_tmp = $this->nextRow($res_tmp);
 
-            self::$res[$res_num]["found_rows"] = $row_tmp["found_rows"];
+            self::$res[$res_num]['found_rows'] = $row_tmp['found_rows'];
         }
-
-        return $res_num;
+        return (int) $res_num;
     }
 
     /**
      *
-     * @param type $query 
+     * @param string $query
+     * @return void
      */
     public function real_query($query) {
-        array_push(self::$history, $query);
+        $this->history[] = $query;
+        $this->last_query = $query;
 
         parent::real_query($query);
 
         if ($this->errno != 0) {
-            throw new Exception("Mysql Error: $this->error<br />Real Query: $query");
+            throw new Exception("Mysql Error: $this->error<br />Real Query: $query", $this->errno);
         }
+
     }
 
     /**
      *
-     * @return type 
+     * @return integer
      */
     public function store_result() {
-        return $this->push_resource(parent::store_result());
+        return (int) $this->push_resource(parent::store_result());
     }
 
     /**
      *
-     * @param type $res_num
-     * @return type 
+     * @param integer $res_num
+     * @return array
      */
     public function nextRow($res_num) {
 
-        $res = self::$res[$res_num]["res"];
+        $res = self::$res[$res_num]['res'];
 
         $row = $res->fetch_assoc();
 
@@ -139,72 +353,100 @@ class Database extends mysqli {
 
     /**
      *
-     * @param type $res_num
-     * @return type 
+     * @param integer $res_num
+     * @return integer
      */
     public function foundRows($res_num) {
-        return self::$res[$res_num]["found_rows"];
+        return (int) self::$res[$res_num]['found_rows'];
     }
 
     /**
      *
-     * @param type $res_num
-     * @return type 
+     * @param integer $res_num
+     * @return integer
      */
     public function numRows($res_num) {
-        return self::$res[$res_num]["num_rows"];
+        return (int) self::$res[$res_num]['num_rows'];
     }
 
     /**
      *
-     * @param type $table
-     * @param type $a_values
-     * @return type 
+     * @param string $table
+     * @param array $a_values
+     * @return integer
      */
-    public function insert($table, $a_values) {
+    public function insert($table, array $a_values) {
         /*
          * $a_values is an array looking like this:
-         * 
+         *
          * array(	"firstname"	=> "John",
          * 			"lastname" 	=> "Doe");
-         * 
-         * So the insert query will be "insert into $table (firstname, lastname) values ('John', 'Doe')" 
+         *
+         * So the insert query will be "INSERT INTO $table (`firstname`, `lastname`) VALUES ('John', 'Doe')"
          */
 
-        $fields = "";
-        $values = "";
+        $fields = '';
+        $values = '';
 
+        // FIXME: Ensure proper escaping of $value, filter $field.
         foreach ($a_values as $field => $value) {
-            $fields .= "$field, ";
+            $fields .= "`{$field}`, ";
             $values .= "'" . addslashes($value) . "', ";
         }
 
-        $fields = substr($fields, 0, strlen($fields) - 2);
-        $values = substr($values, 0, strlen($values) - 2);
+        $s_fields = substr($fields, 0, strlen($fields) - 2);
+        $s_values = substr($values, 0, strlen($values) - 2);
 
-        $query = "insert into $table ($fields) values ($values)";
+        $query = "INSERT INTO $table ($s_fields) VALUES ($s_values)";
+
         $this->query($query);
-        return $this->insert_id;
+
+        return (int) $this->insert_id;
     }
 
     /**
      *
-     * @param type $table
-     * @param type $a_values
-     * @param type $where 
+     * @param string $table
+     * @param array $a_values
+     * @param mixed $where
+     * @return integer
      */
-    public function update($table, $a_values, $where) {
+    public function update($table, array $a_values, $where, $where_val = null) {
         /*
          * $a_values is an array. Check the "insert" function above for more details.
          */
-        $fields = "";
-        foreach ($a_values as $field => $value) {
-            $fields .= "$field='" . addslashes($value) . "', ";
+
+        $fields = '';
+
+        foreach ($a_values as $field => $value){
+            $fields .= "`{$field}` = '" . addslashes($value) . "', ";
         }
-        $fields = substr($fields, 0, strlen($fields) - 2);
-        $query = "update $table set $fields $where";
+
+        $s_fields = substr($fields, 0, strlen($fields) - 2);
+
+        if (null == $where_val) {
+            $query = "UPDATE $table SET $s_fields $where";
+        } else {
+            // FIXME: Ensure escaping of $where_val by type
+            $query = "UPDATE $table SET $s_fields WHERE `$where` = '$where_val' ";
+        }
+
         $this->query($query);
+
+        return (int) $this->affected_rows;
     }
+
+    /**
+     *
+     * @param string $sp
+     * @param array $params
+     * @return mixed
+     */
+    public function call($sp, $params) {
+        return $this->$sp($params);
+    }
+
+    // -------------------------------------------------------------------------
 
     public function get() {
         // TODO: Implement
@@ -212,16 +454,6 @@ class Database extends mysqli {
     
     public function delete() {
         // TODO: Implement
-    }
-
-    /**
-     *
-     * @param type $sp
-     * @param type $params
-     * @return type 
-     */
-    public function call($sp, $params) {
-        return $this->$sp($params);
     }
 }
 
