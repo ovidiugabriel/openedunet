d92a781920baf88ce8d4ae07b02315ebfc8b3d47
Ovidiu <ovidiugabriel@gmail.com>
2016-09-23 16:11:06 +0300

    Update Action.hx

--- bbmvc/includes/haxe/play/mvc/Action.hx
+++ bbmvc/includes/haxe/play/mvc/Action.hx
@@ -1,5 +1,10 @@
 
 package play.mvc;
 
-class Action {
+extern class Action<T> extends Results {
+    var configuration:T;
+    private function new(); /* abstract class */
+
+    // abstract java.util.concurrent.CompletionStage<Result> 	call(Http.Context ctx)
+    public function call(ctx:Dynamic):Dynamic;
 }
