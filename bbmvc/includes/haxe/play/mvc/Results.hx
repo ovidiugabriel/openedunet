840fe2b5ae61c6500771c4089696053f9988e88e
Ovidiu <ovidiugabriel@gmail.com>
2016-09-25 01:39:49 +0300

    Update Results.hx

--- bbmvc/includes/haxe/play/mvc/Results.hx
+++ bbmvc/includes/haxe/play/mvc/Results.hx
@@ -68,232 +68,136 @@ class Results {
         #end
     }
 
+    /**
+        Generates a 302 Found result.
+     **/
     static public function found(Call call):Result;
-    // Generates a 302 Found result.
-
     static public function found(java.lang.String url):Result {
     }
 
-    static public function 	internalServerError():StatusHeader;
-    // Generates a 500 Internal Server Error result.
-    
+    /** 
+        Generates a 500 Internal Server Error result.
+     **/
+    static public function internalServerError():StatusHeader;
     static public function internalServerError(byte[] content):Result;
-    // Generates a 500 Internal Server Error result.
-
     static public function internalServerError(play.twirl.api.Content content):Result;
-    // Generates a 500 Internal Server Error result.
-
     static public function internalServerError(play.twirl.api.Content content, java.lang.String charset):Result;
-    // Generates a 500 Internal Server Error result.
-    
     static public function internalServerError(java.io.File content):Result;
-    // Generates a 500 Internal Server Error result.
-    
     static public function internalServerError(java.io.File content, boolean inline):Result;
-    // Generates a 500 Internal Server Error result.
-    
     static public function internalServerError(java.io.File content, java.lang.String filename):Result;
-    // Generates a 500 Internal Server Error result.
-
     static public function internalServerError(java.io.InputStream content):Result;
-    // Generates a 500 Internal Server Error result.
-    
     static public function internalServerError(java.io.InputStream content, long contentLength):Result;
-    // Generates a 500 Internal Server Error result.
-
     static public function internalServerError(com.fasterxml.jackson.databind.JsonNode content):Result;
-    // Generates a 500 Internal Server Error result.
-    
     static public function internalServerError(com.fasterxml.jackson.databind.JsonNode content, com.fasterxml.jackson.core.JsonEncoding encoding):Result;
-    // Generates a 500 Internal Server Error result.
-    
-    static public function internalServerError(com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;
-    // Generates a 500 Internal Server Error result.
-
-    static public function internalServerError(Results.Chunks<?> chunks):Result;
-    // Deprecated. 
-    // Use internalServerError() with StatusHeader.chunked(akka.stream.javadsl.Source) instead.
-    
+    static public function internalServerError(com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;   
     static public function internalServerError(java.lang.String content):Result;
-    // Generates a 500 Internal Server Error result.
-
     static public function internalServerError(java.lang.String content, java.lang.String charset):Result {
-    }
-
-    static public function movedPermanently(Call call):Result;
-    // Generates a 301 Moved Permanently result.
-
-    static public function movedPermanently(java.lang.String url):Result {
         #if java
         #end
     }
 
-    // Generates a 204 No Content result.
-    static public function noContent():StatusHeader {}
-    
+    /**
+        Generates a 301 Moved Permanently result.
+     **/
+    @:overload(function(call:Call):Result{})
+    static public function movedPermanently(url:String):Result {
+        #if java
+        #end
+    }
 
+    /**
+        Generates a 204 No Content result.
+     **/
+    static public function noContent():StatusHeader {
+        #if java
+        #end        
+    }   
+    
+    /**
+        Generates a 404 Not Found result.
+     **/
     static public function notFound():StatusHeader;
-    // Generates a 404 Not Found result.
-
     static public function notFound(byte[] content):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(play.twirl.api.Content content):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(play.twirl.api.Content content, java.lang.String charset):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(java.io.File content):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(java.io.File content, boolean inline):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(java.io.File content, java.lang.String filename):Result;
-    // Generates a 404 Not Found result.
-    
     static public function notFound(java.io.InputStream content):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(java.io.InputStream content, long contentLength):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(com.fasterxml.jackson.databind.JsonNode content):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(com.fasterxml.jackson.databind.JsonNode content, com.fasterxml.jackson.core.JsonEncoding encoding):Result;
-    // Generates a 404 Not Found result.
-
     static public function notFound(com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;
-    // Generates a 404 Not Found result.
-
-    static public function notFound(Results.Chunks<?> chunks):Result;
-    // Deprecated. 
-    // Use notFound() with StatusHeader.chunked(akka.stream.javadsl.Source) instead.
-
-    static public function notFound(content:String):Result;
-    // Generates a 404 Not Found result.
-    
+    static public function notFound(content:String):Result;   
     static public function notFound(java.lang.String content, java.lang.String charset):Result {
         #if java
         #end
     }
     
-
+    /**
+        Generates a 200 OK result.
+     **/
     static public function ok():StatusHeader;
-    // Generates a 200 OK result.
-
     static public function ok(byte[] content):Result;
-    // Generates a 200 OK result.
-
     static public function ok(play.twirl.api.Content content):Result;
-    // Generates a 200 OK result.
-
     static public function ok(play.twirl.api.Content content, java.lang.String charset):Result;
-    // Generates a 200 OK result.
-
     static public function ok(java.io.File content):Result;
-    // Generates a 200 OK result.
-
     static public function ok(java.io.File content, boolean inline):Result;
-    // Generates a 200 OK result.
-
     static public function ok(java.io.File content, java.lang.String filename):Result;
-    // Generates a 200 OK result.
-
     static public function ok(java.io.InputStream content):Result;
-    // Generates a 200 OK result.
-
     static public function ok(java.io.InputStream content, long contentLength):Result;
-    // Generates a 200 OK result.
-
     static public function ok(com.fasterxml.jackson.databind.JsonNode content):Result;
-    // Generates a 200 OK result.
-
     static public function ok(com.fasterxml.jackson.databind.JsonNode content, com.fasterxml.jackson.core.JsonEncoding encoding):Result;
-    // Generates a 200 OK result.
-
-    static public function ok(com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;
-    // Generates a 200 OK result.
-
-    static public function ok(Results.Chunks<?> chunks):Result;
-    // Deprecated. 
-    // Use ok() with StatusHeader.chunked(akka.stream.javadsl.Source) instead.
-    
+    static public function ok(com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;   
     static public function ok(content:String):Result;
-    // Generates a 200 OK result.
-
     static public function ok(content:String, charset:String):Result {
         #if java
         #end
     }
     
+    /**
+        Generates a 402 Payment Required result.
+     **/
     static public function paymentRequired():StatusHeader;
-    // Generates a 402 Payment Required result.
-
     static public function paymentRequired(byte[] content):Result;
-    // Generates a 402 Payment Required result.
-
     static public function paymentRequired(play.twirl.api.Content content):Result;
-    // Generates a 402 Payment Required result.
-
     static public function paymentRequired(play.twirl.api.Content content, java.lang.String charset):Result;
-    // Generates a 402 Payment Required result.
-
     static public function paymentRequired(java.io.File content):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(java.io.File content, boolean inline):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(java.io.File content, java.lang.String filename):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(java.io.InputStream content):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(java.io.InputStream content, long contentLength):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(com.fasterxml.jackson.databind.JsonNode content):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(com.fasterxml.jackson.databind.JsonNode content, com.fasterxml.jackson.core.JsonEncoding encoding):Result;
-    // Generates a 402 Payment Required result.
-
-    static public function 	paymentRequired(com.fasterxml.jackson.databind.JsonNode content, charset:String):Result;
-    // Generates a 402 Payment Required result.
-
-    static public function 	paymentRequired(Results.Chunks<?> chunks):Result;
-    // Deprecated. 
-    // Use paymentRequired() with StatusHeader.chunked(akka.stream.javadsl.Source) instead.
-    
+    static public function 	paymentRequired(com.fasterxml.jackson.databind.JsonNode content, charset:String):Result;   
     static public function 	paymentRequired(content:String):Result;
-    // Generates a 402 Payment Required result.
-
     static public function 	paymentRequired(content:String, charset:String):Result {
         #if java
         #end
     }
 
+    /**
+        Generates a 308 Permanent Redirect result.
+     **/
     static public function 	permanentRedirect(Call call):Result;
-    // Generates a 308 Permanent Redirect result.
-
     static public function 	permanentRedirect(url:String):Result {
         #if java
         #end        
     }
 
+    /**
+        Generates a 303 See Other result.
+     **/
     static public function 	redirect(Call call):Result;
-    // Generates a 303 See Other result.
-
     static public function 	redirect(url:String):Result {
+        #if java
+        #end        
     }
 
+    /**
+        Generates a 303 See Other result.
+     **/
     static public function 	seeOther(Call call):Result;
-    // Generates a 303 See Other result.
-
     static public function 	seeOther(url:String):Result {
         #if java
         #end        
@@ -302,108 +206,50 @@ class Results {
     /**
         Generates a simple result.
      **/
-    static public function 	status(status:Int):StatusHeader;
-    // Generates a simple result.
-
-    static public function 	status(status:Int, byte[] content):Result;
-    // Generates a simple result with byte-array content.
-
+    static public function status(status:Int):StatusHeader;
+    static public function status(status:Int, byte[] content):Result;
     static public function status(status:Int, akka.util.ByteString content):Result;
-    // Generates a simple result.
-
-    static public function 	status(status:Int, play.twirl.api.Content content):Result;
-    // Generates a simple result.
-
+    static public function status(status:Int, play.twirl.api.Content content):Result;
     static public function status(status:Int, play.twirl.api.Content content, java.lang.String charset):Result;
-    // Generates a simple result.
-
     static public function status(status:Int, java.io.File content):Result;
-    // Generates a result with file contents.
-
     static public function status(status:Int, java.io.File content, boolean inline):Result;
-    // Generates a result with file content.
-
     static public function status(status:Int, java.io.File content, java.lang.String fileName):Result;
-    // Generates a result.
-
     static public function status(status:Int, java.io.InputStream content):Result;
-    // Generates a chunked result.
-
     static public function status(status:Int, java.io.InputStream content, long contentLength):Result;
-    // Generates a chunked result.
-
     static public function status(status:Int, com.fasterxml.jackson.databind.JsonNode content):Result;
-    // Generates a simple result with json content and UTF8 encoding.
-
     static public function status(status:Int, com.fasterxml.jackson.databind.JsonNode content, com.fasterxml.jackson.core.JsonEncoding encoding):Result;
-    // Generates a simple result with json content.
-
     static public function status(status:Int, com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;
-    // Generates a simple result with json content.
-
-    static public function status(status:Int, Results.Chunks<?> chunks):Result;
     static public function status(status:Int, content:String):Result;
-    // Generates a simple result.
-    
-
     static public function status(status:Int, content:String, charset:String):Result {
         #if java
         #end        
     }
     
-    
+    /**
+        Generates a 307 Temporary Redirect result.
+     **/
     static public function temporaryRedirect(Call call):Result;
-    // Generates a 307 Temporary Redirect result.
-
     static public function temporaryRedirect(java.lang.String url):Result {
         #if java
         #end        
     }
 
+    /**
+        Generates a 401 Unauthorized result.
+     **/
     static public function unauthorized():StatusHeader;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(byte[] content):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(play.twirl.api.Content content):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(play.twirl.api.Content content, java.lang.String charset):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(java.io.File content):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(java.io.File content, boolean inline):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(java.io.File content, java.lang.String filename):Result;
-    // Generates a 401 Unauthorized result.
-    
     static public function unauthorized(java.io.InputStream content):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(java.io.InputStream content, long contentLength):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(com.fasterxml.jackson.databind.JsonNode content):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(com.fasterxml.jackson.databind.JsonNode content, com.fasterxml.jackson.core.JsonEncoding encoding):Result;
-    // Generates a 401 Unauthorized result.
-
     static public function unauthorized(com.fasterxml.jackson.databind.JsonNode content, java.lang.String charset):Result;
-    // Generates a 401 Unauthorized result.
-
-    static public function unauthorized(Results.Chunks<?> chunks):Result;
-    // Deprecated. 
-    // Use unauthorized() with StatusHeader.chunked(akka.stream.javadsl.Source) instead.
-
-    // Generates a 401 Unauthorized result.
     static public function unauthorized(java.lang.String content):Result;
-
-    // Generates a 401 Unauthorized result.
     static public function unauthorized(java.lang.String content, java.lang.String charset):Result {
         #if java
         #end
