a37cf827b2267946aaaf8fb53bd787c8d4fff71d
Ovidiu <ovidiugabriel@gmail.com>
2016-09-25 16:27:34 +0300

    Update Results.hx

--- bbmvc/includes/haxe/play/mvc/Results.hx
+++ bbmvc/includes/haxe/play/mvc/Results.hx
@@ -71,8 +71,7 @@ class Results {
     @:overload(function(content:InputStream, ?contentLength:Int):Result{})
     @:overload(function(content:JsonNode, ?encoding:JsonEncoding):Result{})
     @:overload(function(content:JsonNode, charset:String):Result{})
-    @:overload(function(content:String):Result{})
-    static public function internalServerError(content:String, charset:String):Result {
+    static public function internalServerError(content:String, ?charset:String):Result {
         #if java
         #end
     }
@@ -97,20 +96,16 @@ class Results {
     /**
         Generates a 404 Not Found result.
      **/
-    static public function notFound():StatusHeader;
-    static public function notFound(byte[] content):Result;
-    static public function notFound(play.twirl.api.Content content):Result;
-    static public function notFound(play.twirl.api.Content content, java.lang.String charset):Result;
-    static public function notFound(java.io.File content):Result;
-    static public function notFound(java.io.File content, boolean inline):Result;
-    static public function notFound(java.io.File content, java.lang.String filename):Result;
-    static public function notFound(java.io.InputStream content):Result;
-    static public function notFound(java.io.InputStream content, long contentLength):Result;
-    static public function notFound(JsonNode content):Result;
-    static public function notFound(JsonNode content, JsonEncoding encoding):Result;
-    static public function notFound(JsonNode content,String charset):Result;
-    static public function notFound(content:String):Result;   
-    static public function notFound(String content, String charset):Result {
+    @:overload(function():StatusHeader{})
+    @:overload(function(content:haxe.io.Bytes):Result{})
+    @:overload(function(content:Content):Result{})
+    @:overload(function(content:Content, charset:String):Result{})
+    @:overload(function(content:File, ?isInline:Bool):Result{})
+    @:overload(function(content:File, filename:String):Result{})
+    @:overload(function(content:InputStream, ?contentLength:Int):Result{})
+    @:overload(function(content:JsonNode, ?encoding:JsonEncoding):Result{})
+    @:overload(function(content:JsonNode, charset:String):Result{})
+    static public function notFound(content:String, ?charset:String):Result {
         #if java
         #end
     }
