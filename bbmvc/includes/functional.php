4dd20d9744db6f8d5c9992133aa5dc86a5d65688
Ovidiu <ovidiugabriel@gmail.com>
2016-09-30 16:49:16 +0300

    Create functional.php

---
+++ bbmvc/includes/functional.php
@@ -0,0 +1,9 @@
+<?php
+
+function foldl($lst, $callback){
+	return array_reduce(arr($lst), $callback);
+}
+
+function foldr($lst, $callback){
+	return array_reduce(array_reverse(arr($lst)), $callback);
+}
