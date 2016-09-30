9897c706ef1dc8a489ea6c80bfe88ee9b8651817
Ovidiu <ovidiugabriel@gmail.com>
2016-09-30 16:56:46 +0300

    Update functional.php

--- bbmvc/includes/functional.php
+++ bbmvc/includes/functional.php
@@ -1,9 +1,30 @@
 <?php
 
-function foldl($lst, $callback){
-    return array_reduce(arr($lst), $callback);
+/** 
+ * @param object|array $lst
+ * @return array
+ */
+function from_pharen_vector($lst) {
+    if (is_object($lst) && get_class($lst) == 'PharenVector') {
+        return arr($lst);
+    }
+    return $lst;
 }
 
+/** 
+ * @param object|array $lst
+ * @param callable $callbakc
+ * @return mixed
+ */
+function foldl($lst, $callback){
+	return array_reduce(from_pharen_vector($lst), $callback);
+}
+
+/** 
+ * @param object|array $lst
+ * @param callable $callback
+ * @return mixed
+ */
 function foldr($lst, $callback){
-    return array_reduce(array_reverse(arr($lst)), $callback);
+	return array_reduce(array_reverse(from_pharen_vector($lst)), $callback);
 }
