3eecd03c6fe08fc053090540f20d5408de545dc4
Ovidiu <ovidiugabriel@gmail.com>
2017-04-19 17:01:44 +0300

    Update ApplicationLoader.hx

--- bbmvc/includes/haxe/play/ApplicationLoader.hx
+++ bbmvc/includes/haxe/play/ApplicationLoader.hx
@@ -1,1 +1,5 @@
 
+// See: https://www.playframework.com/documentation/2.5.x/api/java/play/ApplicationLoader.html
+interface ApplicationLoader {
+    public function load(context:Context):Application;
+}
