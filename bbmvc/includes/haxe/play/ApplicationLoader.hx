69c77575a3aa1c5b25ec7570c72e692cff296337
Ovidiu <ovidiugabriel@gmail.com>
2017-04-19 17:41:08 +0300

    Update ApplicationLoader.hx

--- bbmvc/includes/haxe/play/ApplicationLoader.hx
+++ bbmvc/includes/haxe/play/ApplicationLoader.hx
@@ -1,5 +1,17 @@
 
 // See: https://www.playframework.com/documentation/2.5.x/api/java/play/ApplicationLoader.html
+
+@:final
+class ApplicationLoader_Context {
+    public function create(environment:Environment):ApplicationLoader_Context {}
+    // create(Environment environment, java.util.Map<java.lang.String,java.lang.Object> initialSettings)
+    public function environment():Environment {}
+    public function initialConfiguration():Configuration {}
+    public function underlying():play.api.ApplicationLoader_Context {}
+    public function withConfiguration(initialConfiguration:Configuration):ApplicationLoader_Context {}
+    public function withEnvironment(environment:Environment):ApplicationLoader_Context {}
+}
+
 interface ApplicationLoader {
-    public function load(context:Context):Application;
+    public function load(context:ApplicationLoader_Context):Application;
 }
