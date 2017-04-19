27ac35f1ee1aefc42aa4ccd2e61d4149f00c9ad6
Ovidiu <ovidiugabriel@gmail.com>
2017-04-19 17:00:22 +0300

    Update Application.hx

--- bbmvc/includes/haxe/play/Application.hx
+++ bbmvc/includes/haxe/play/Application.hx
@@ -3,5 +3,14 @@
 
 interface Application {
     public function classloader():ClassLoader;
-    
+    public function configuration():Configuration;
+    public function getFile(relativePath:String):File;
+    public function getWrappedApplication():Application;
+    public function injector():Injector;
+    public function isDev():Bool;
+    public function isProd():Bool;
+    public function isTest():Bool;
+    public function path():File;
+    public function resource(relativePath:String):URL;
+    public function resourceAsStream(java.lang.String relativePath:String):InputStream;
 }
