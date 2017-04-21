2458e252ff16d906c0111c0f9e19ce0ba82fc90a
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 16:07:50 +0300

    Update DelegatingMultipartFormDataBodyParser.hx

--- bbmvc/includes/haxe/play/mvc/bodyparser/DelegatingMultipartFormDataBodyParser.hx
+++ bbmvc/includes/haxe/play/mvc/bodyparser/DelegatingMultipartFormDataBodyParser.hx
@@ -3,7 +3,10 @@ package play.mvc.bodyparser;
 
 // A body parser that exposes a file part handler as an abstract method and delegates the implementation 
 // to the underlying Scala multipartParser.
-class DelegatingMultipartFormDataBodyParser<A> {
-    @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
-    public function new(maxLength:Int64, errorHandler:HttpErrorHandler) {}
+class DelegatingMultipartFormDataBodyParser<A> implements BodyParser<MultipartFormData<A>> {
+    public function new(materializer:Materializer, maxLength:Int64){}
+    public function createFilePartHandler():Function<FileInfo, Accumulator<ByteString, FilePart<A>>> {
+        /* abstract */
+    }            
+    public function apply(request:RequestHeader):Accumulator<ByteString, Either<Result, MultipartFormData<A>>> {}
 }
