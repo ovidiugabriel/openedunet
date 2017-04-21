b5befa837b1c5094169df4afcb42379f0b65c86b
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 15:50:20 +0300

    Update BodyParser.hx

--- bbmvc/includes/haxe/play/mvc/BodyParser.hx
+++ bbmvc/includes/haxe/play/mvc/BodyParser.hx
@@ -9,14 +9,6 @@ interface BodyParser<A> {
 
 package play.mvc.bodyparser;
 
-// If PATCH, POST, or PUT, guess the body content by checking the Content-Type header.
-class Default {
-    @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
-    public function new(maxLength:Int64, errorHandler:HttpErrorHandler) {}
-}
-
-package play.mvc.bodyparser;
-
 // A body parser that delegates to a Scala body parser, and uses the supplied function to transform its result to a Java body.
 class DelegatingBodyParser<A,B> {
     @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
