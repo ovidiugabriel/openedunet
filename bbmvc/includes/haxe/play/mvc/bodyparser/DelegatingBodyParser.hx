0c7ce0b4d590f5b1526cc423ad138ff5706ef2b0
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 16:04:39 +0300

    Update DelegatingBodyParser.hx

--- bbmvc/includes/haxe/play/mvc/bodyparser/DelegatingBodyParser.hx
+++ bbmvc/includes/haxe/play/mvc/bodyparser/DelegatingBodyParser.hx
@@ -2,7 +2,7 @@
 package play.mvc.bodyparser;
 
 // A body parser that delegates to a Scala body parser, and uses the supplied function to transform its result to a Java body.
-class DelegatingBodyParser<A,B> {
-    @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
-    public function new(maxLength:Int64, errorHandler:HttpErrorHandler) {}
+class DelegatingBodyParser<A,B> implements BodyParser<A> {
+    public function new(delegate:BodyParser<B>, transform:Function<B,A>){}
+    public function apply(request:RequestHeader):Accumulator<ByteString, Either<Result,A>>{}
 }
