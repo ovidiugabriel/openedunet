5e4a52c1993ce6235fb7ddff376595691fd80c31
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 15:38:48 +0300

    Update BufferingBodyParser.hx

--- bbmvc/includes/haxe/play/mvc/bodyparser/BufferingBodyParser.hx
+++ bbmvc/includes/haxe/play/mvc/bodyparser/BufferingBodyParser.hx
@@ -1,1 +1,12 @@
 
+package play.mvc.bodyparser;
+
+// A body parser that first buffers
+class BufferingBodyParser<A> {
+    private function new(maxLength:Int64, errorHandler:HttpErrorHandler, errorMessage:String) {}
+    private function new(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler, errorMessage:String) {}
+
+    @:final
+    private function apply(request:RequestHeader):Accumulator<ByteString, Either<Result,A>>{}
+    private function parse(request:RequestHeader, bytes:ByteString):A { /* abstract */ }
+}
