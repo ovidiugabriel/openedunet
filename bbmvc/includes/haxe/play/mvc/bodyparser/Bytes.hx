f89905ce4257d2d9c4b6bb550b4bc1cccf693055
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 15:39:58 +0300

    Update Bytes.hx

--- bbmvc/includes/haxe/play/mvc/bodyparser/Bytes.hx
+++ bbmvc/includes/haxe/play/mvc/bodyparser/Bytes.hx
@@ -1,1 +1,8 @@
 
+package play.mvc.bodyparser;
+
+// Parse the body as a byte string.
+class Bytes {
+    @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
+    public function new(maxLength:Int64, errorHandler:HttpErrorHandler) {}
+}
