4698898b8b9eb867e78e9e9d3bc32e0811793fc4
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 15:37:48 +0300

    Update AnyContent.hx

--- bbmvc/includes/haxe/play/mvc/bodyparser/AnyContent.hx
+++ bbmvc/includes/haxe/play/mvc/bodyparser/AnyContent.hx
@@ -1,1 +1,8 @@
 
+package play.mvc.bodyparser;
+
+// Guess the body content by checking the Content-Type header.
+class AnyContent {
+    public function new( errorHandler:HttpErrorHandler,  httpConfiguration:HttpConfiguration) {}
+    public function apply(request:RequestHeader):Accumulator<ByteString, Either<Result,Object>>{}
+}
