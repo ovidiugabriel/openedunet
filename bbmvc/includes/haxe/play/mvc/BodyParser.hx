70e82c7ab10bc5e8e0b7e4c5bf17d5b9e415ee72
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 15:40:26 +0300

    Update BodyParser.hx

--- bbmvc/includes/haxe/play/mvc/BodyParser.hx
+++ bbmvc/includes/haxe/play/mvc/BodyParser.hx
@@ -9,14 +9,6 @@ interface BodyParser<A> {
 
 package play.mvc.bodyparser;
 
-// Parse the body as a byte string.
-class Bytes {
-    @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
-    public function new(maxLength:Int64, errorHandler:HttpErrorHandler) {}
-}
-
-package play.mvc.bodyparser;
-
 // If PATCH, POST, or PUT, guess the body content by checking the Content-Type header.
 class Default {
     @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
