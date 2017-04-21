923b1a107b1bc07b91c9f24bfe066562f93f43ca
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 16:02:24 +0300

    Update Default.hx

--- bbmvc/includes/haxe/play/mvc/bodyparser/Default.hx
+++ bbmvc/includes/haxe/play/mvc/bodyparser/Default.hx
@@ -2,7 +2,7 @@
 package play.mvc.bodyparser;
 
 // If PATCH, POST, or PUT, guess the body content by checking the Content-Type header.
-class Default {
-    @:overload(function(httpConfiguration:HttpConfiguration, errorHandler:HttpErrorHandler) {})
-    public function new(maxLength:Int64, errorHandler:HttpErrorHandler) {}
+class Default extends AnyContent {
+    public function new(errorHandler:HttpErrorHandler, httpConfiguration:HttpConfiguration) {}
+    public function apply(request:RequestHeader):Accumulator<ByteString, Either<Result, Object>>{}
 }
