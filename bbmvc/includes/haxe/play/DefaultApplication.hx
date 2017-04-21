895715e2dd8fa42459ad48425eabc5787a269aba
Ovidiu <ovidiugabriel@gmail.com>
2017-04-21 11:36:33 +0300

    Update DefaultApplication.hx

--- bbmvc/includes/haxe/play/DefaultApplication.hx
+++ bbmvc/includes/haxe/play/DefaultApplication.hx
@@ -1,4 +1,6 @@
 
+// See: https://www.playframework.com/documentation/2.5.x/api/java/index.html?play/package-tree.html
+
 class DefaultApplication {
     public function configuration():Configuration{}
     public function getWrappedApplication():play.api.Application{}
