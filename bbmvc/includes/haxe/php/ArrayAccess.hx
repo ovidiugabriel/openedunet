b6af2a526d3a2b429b23eaafa70b754e5817762a
Ovidiu <ovidiugabriel@gmail.com>
2017-04-27 14:28:55 +0300

    Update ArrayAccess.hx

--- bbmvc/includes/haxe/php/ArrayAccess.hx
+++ bbmvc/includes/haxe/php/ArrayAccess.hx
@@ -1,8 +1,10 @@
 
 package php;
 
+// Provides a class that can be accessed using brackets.
+// This is used for the PHP runtime.
 interface ArrayAccess {
-    public function boolean offsetExists ( offset : Dynamic );
+    public function offsetExists ( offset : Dynamic ) : Bool;
     public function offsetGet ( offset : Dynamic ) : Dynamic;
     public function offsetSet ( offset : Dynamic , value :Dynamic ) : Void;
     public function offsetUnset ( offset : Dynamic ) : Void;
