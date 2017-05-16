ccde5f1e88a87549e62490064a1db20a019ddda9
Ovidiu <ovidiugabriel@gmail.com>
2017-05-16 10:35:45 +0300

    Update GUID.hx

--- bbmvc/includes/haxe/GUID.hx
+++ bbmvc/includes/haxe/GUID.hx
@@ -8,6 +8,15 @@ abstract GUID(String) {
     public var Data3(get, default):haxe.Int32;          // WORD
     public var Data4(get, default):haxe.io.UInt8Array;  // BYTE[8]
     
-    public function get_Data1() : haxe.Int32 {
+    public function get_Data1():haxe.Int32 {
+    }
+    
+    public function get_Data2():haxe.Int32 {
+    }
+    
+    public function get_Data3():haxe.Int32 {
+    }
+    
+    public function get_Data4():haxe.io.UInt8Array {
     }
 }
