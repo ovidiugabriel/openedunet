b17a1262fee0b7fc4fb4a52816ae50a5195f42c8
Ovidiu <ovidiugabriel@gmail.com>
2018-04-05 11:02:07 +0300

    Update ClassLoader.hx

--- bbmvc/includes/haxe/ClassLoader.hx
+++ bbmvc/includes/haxe/ClassLoader.hx
@@ -4,6 +4,15 @@
 //    https://github.com/ovidiugabriel/openedunet/blob/master/bbmvc/includes/ClassLoader.class.php
 //
 extern class ClassLoader {
+    static public function getResource(name : String) : String;
+    static public function register() : Void;
+    static public function removeClass(className : String) : Void;
+    static public function autoload(className : String) : Void;
     static public function _import(name : String, ?callback : Dynamic) : Void;
     static public function createInstance(name : String) : Dynamic;
+    static public function getInstance(?name : String = null, ?singleton : Bool = true) : Dynamic;
+    static public function singleton(name : String) : Dynamic;
+    static public function requireObject(name : String, ?fn : Dynamic = null) : Dynamic;
+    static public function requireClass(name : String, ?fn : Dynamic = null) : Dynamic;
+    static public function addIncludePath(path : String) : Void;
 }
