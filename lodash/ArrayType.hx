31420ee82a006bb0f5df539e7ba8bf4173c1f117
Ovidiu <ovidiugabriel@gmail.com>
2018-11-20 13:27:08 +0200

    Update ArrayType.hx

--- lodash/ArrayType.hx
+++ lodash/ArrayType.hx
@@ -1,13 +1,49 @@
 
 package lodash;
 
-abstract ArrayType(Array<Variant>) {
+
+abstract ArrayType(Array<Dynamic>) {
+    /**
+        [static] fromIntArray()
+    **/
     @:from
-    static public function fromIntArray(value : Array<Int>) {
+    static public function fromIntArray(value: Array<Dynamic>): ArrayType {
         return new ArrayType(value);
     }
 
-    public function new(value : Array<Variant>) {
+    /**
+        new()
+    **/
+    public function new(value: Array<Dynamic>) {
         this = value;
+    }
+
+    /**
+        copy()
+    **/
+    public function copy(): ArrayType {
+        return this.copy();
+    }
+
+    /**
+        push()
+    **/
+    public function push(element: Dynamic): Int {
+        return this.push(element);
+    }
+
+    /**
+        length()
+    **/
+    public function length(): Int {
+        return this.length;
+    }
+
+    /**
+        toArray()
+    **/
+    @:to
+    public function toArray(): Array<Dynamic> {
+        return this;
     }
 }
