5e4fc2b0f754e06dc4e79d2588613c1ea664b1e4
Ovidiu <ovidiugabriel@gmail.com>
2018-11-20 14:16:51 +0200

    Update Lo.hx

--- lodash/Lo.hx
+++ lodash/Lo.hx
@@ -62,7 +62,34 @@ class Lo {
         #if js
         return lo().values;
         #end
-    }    
+    }
+
+    /**
+        Creates a function that memoizes the result of func. If resolver is provided, 
+        it determines the cache key for storing the result based on the arguments 
+        provided to the memoized function. By default, the first argument provided 
+        to the memoized function is used as the map cache key. The func is invoked 
+        with the this binding of the memoized function.
+
+        Note: The cache is exposed as the cache property on the memoized function. 
+        Its creation may be customized by replacing the _.memoize.Cache constructor 
+        with one whose instances implement the Map method interface of clear, 
+        delete, get, has, and set.
+
+        Arguments
+
+            func (Function): The function to have its output memoized.
+            [resolver] (Function): The function to resolve the cache key.
+
+        Returns
+
+            (Function): Returns the new memoized function.
+    **/
+    static inline public function get_memoize() : Dynamic {
+        #if js
+        return lo().memoize;
+        #end
+    }
     
     /** 
         Creates an array of elements split into groups the length of size. 
