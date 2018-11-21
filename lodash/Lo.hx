c9845d99626d61835a8cf263126ee6b754deb0db
Ovidiu <ovidiugabriel@gmail.com>
2018-11-21 16:50:03 +0200

    Updated indexOf() method

--- lodash/Lo.hx
+++ lodash/Lo.hx
@@ -523,11 +523,25 @@ class Lo {
     }
 
     /**
-        Gets the index at which the first occurrence of value is found in array 
-        using SameValueZero for equality comparisons. If fromIndex is negative, 
+        Gets the index at which the first occurrence of value is found in array
+        using SameValueZero for equality comparisons. If fromIndex is negative,
         it's used as the offset from the end of array.
+
+        Arguments
+
+            array (Array): The array to inspect.
+            value (*): The value to search for.
+            [fromIndex=0] (number): The index to search from.
+
+        Returns
+
+            (number): Returns the index of the matched value, else -1.
     **/
-    public static function indexOf(array: ArrayType, value: Variant, ?fromIndex: Int = 0): Int;
+    public static function indexOf(array: ArrayType, value: Dynamic, ?fromIndex: Int = 0): Int {
+        #if js
+            return lo().indexOf(array, value, fromIndex);
+        #end
+    }
 
     /**
         Gets all but the last element of array.
