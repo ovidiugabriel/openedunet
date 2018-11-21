4d55503ce543cd10d2fe37ad641a6d73600654de
Ovidiu <ovidiugabriel@gmail.com>
2018-11-21 16:03:39 +0200

    Updated dropRightWhile() method

--- lodash/Lo.hx
+++ lodash/Lo.hx
@@ -328,11 +328,25 @@ class Lo {
     }
 
     /**
-        Creates a slice of array excluding elements dropped from the end. 
-        Elements are dropped until predicate returns falsey. 
+        Creates a slice of array excluding elements dropped from the end.
+        Elements are dropped until predicate returns falsey.
         The predicate is invoked with three arguments: (value, index, array).
+
+        Arguments
+
+            array (Array): The array to query.
+            [predicate=_.identity] (Function): The function invoked per iteration.
+
+        Returns
+
+        (Array): Returns the slice of array.
     **/
-    public static function dropRightWhile(array: ArrayType, ?predicate: Predicate): ArrayType;
+    @:overload(function (array : ArrayType, ?predicate : Dynamic) : ArrayType {})
+    public static function dropRightWhile(array : ArrayType, ?predicate : Predicate) : ArrayType {
+        #if js
+            return lo().dropRightWhile(array, predicate);
+        #end
+    }
 
     /**
         Creates a slice of array excluding elements dropped from the beginning. 
