e73b9e61a5befadc0b91d2a8aab25fdda6357ab9
Ovidiu <ovidiugabriel@gmail.com>
2019-03-14 16:20:25 +0200

    Update Seq.hx

--- lodash/Seq.hx
+++ lodash/Seq.hx
@@ -1,6 +1,40 @@
 
 package lodash;
 
-class Seq {
-    
+abstract Seq(ArrayType) {
+    public function at() {
+
+    }
+
+    public function chain() {
+
+    }
+
+    public function commit() {
+
+    }
+
+    public function next() {
+
+    }
+
+    public function plant() {
+
+    }
+
+    public function reverse() {
+
+    }
+
+    public function value() {
+
+    }
+
+    public function toJSON() {
+
+    }
+
+    public function valueOf() {
+
+    }
 }
