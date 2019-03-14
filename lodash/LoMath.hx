8d5cef3e7ed23bde3ce29d6b4f46e03a9e2fbefb
Ovidiu <ovidiugabriel@gmail.com>
2019-03-14 16:27:04 +0200

    Update and rename Math.hx to LoMath.hx

---
+++ lodash/LoMath.hx
@@ -0,0 +1,8 @@
+
+package lodash;
+
+class LoMath {
+    public static function add(x: Float, y: Float): Float {
+        return Lo.lo().add(x, y);
+    }
+}
--- lodash/Math.hx
+++
@@ -1,6 +0,0 @@
-
-package lodash;
-
-class Math {
-    
-}
