e77c45085eb4dc8f063fa3934304cd1c2e9f9624
Ovidiu <ovidiugabriel@gmail.com>
2019-03-14 16:28:20 +0200

    Update Function.hx

--- lodash/Function.hx
+++ lodash/Function.hx
@@ -1,6 +1,25 @@
 
 package lodash;
 
-class Function {
+@:callable
+abstract Function(Dynamic) from Dynamic {
+    public var cache(get, never) : Dynamic;
 
+    public function get_cache() : Dynamic {
+        return this.cache;
+    }
+
+    @:from
+    public function new(value: Dynamic) {
+        this = value;
+    }
+
+    /**
+        This will inline the call to the extern object
+        (it is not performing a recursive call)
+    **/
+    @:extern
+    inline public function cancel() {
+        return this.cancel();
+    }
 }
